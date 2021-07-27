5f59518940bba8021955ec4aee419cff6e91c5ef
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2021-07-27 18:47:20 -0300

    Adiciona do requisito 2 ao 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,65 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    /* const { searchText,
+    const {
+      searchText,
       onSearchTextChange,
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
       onSelectedGenreChange,
-     } = this.props; */
+    } = this.props;
 
     return (
-      <form data-testid="earch-bar-form">
-        <select>
-          <option>Item 1</option>
-          <option>Item 2</option>
-        </select>
+      <form data-testid="search-bar-form">
+        <label data-testid="select-input-label" htmlFor="gênero">
+          Filtrar por gênero
+          <select
+            id="genêro"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
+        <label data-testid="text-input-label" htmlFor="text">
+          Inclui o texto
+          <input
+            id="text"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+        <label data-testid="checkbox-input-label" htmlFor="favoritos">
+          Mostrar somente favoritos
+          <input
+            id="favoritos"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
       </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
 export default SearchBar;
