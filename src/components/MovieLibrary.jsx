8b5129b21d4f567dc7860a19ae3359511909b3b8
Pedro Muniz <pedro.muniz@aluno.uece.br>
2021-07-27 16:14:30 -0300

    fix lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/* import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
@@ -14,7 +14,7 @@ class MovieLibrary extends Component {
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={ this.props.movies } />
+         <MovieList movies={ this.props.movies } /> 
         <AddMovie />
       </div>
     );
@@ -27,4 +27,4 @@ MovieLibrary.propTypes = {
   ).isRequired,
 };
 
-export default MovieLibrary;
+export default MovieLibrary; */
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,17 +3,24 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
-    
+    const { searchText, 
+      onSearchTextChange, 
+      // bookmarkedOnly, 
+      // onBookmarkedChange, 
+      // selectedGenre, 
+      // onSelectedGenreChange 
+    } = this.props;
+
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label">
+        <label data-testid="text-input-label" htmlFor="barraBusca">
           Inclui o texto:
           <input
-          data-testid="text-input"
-          value={ searchText }
-          onChange={ onSearchTextChange }
-          type="text"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            type="text"
+            name="barraBusca"
           />
         </label>
       </form>
