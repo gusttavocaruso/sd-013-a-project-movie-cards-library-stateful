4f3fca6e1ac14aea3002972c0f6413b24bd953e5
Yuri Garrido <y.garrido.yuri@gmail.com>
2021-07-27 18:12:28 -0300

    envia o requisito 05, sem lynt

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
+  // eslint-disable-next-line max-lines-per-function
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly, 
-    onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <div>
         <form data-testid="search-bar-form">
@@ -19,8 +20,65 @@ class SearchBar extends React.Component {
               data-testid="text-input"
               type="text"
               id="input-filter"
-              value="searchText"
+              value={ searchText }
             />
+          </label>
+          <label data-testid="checkbox-input-label" htmlFor="checkbox">
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              data-testid="checkbox-input"
+              id="checkbox"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+            />
+          </label>
+          <label data-testid="select-input-label" htmlFor="select-form">
+            Filtrar por gênero
+            <select
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+              name=""
+              id="select-form"
+              data-testid="select-input"
+            >
+              <option
+                data-testid="select-option"
+                name="todos"
+                id="todos"
+                value=""
+              >
+                Todos
+              </option>
+
+              <option
+                data-testid="select-option"
+                name="acao"
+                id="acao"
+                value="action"
+              >
+                Ação
+              </option>
+
+              <option
+                name="comedy"
+                data-testid="select-option"
+                id="comedy"
+                value="comedy"
+              >
+                Comédia
+              </option>
+
+              <option
+                name="suspense"
+                data-testid="select-option"
+                id="suspense"
+                value="thriller"
+              >
+                Suspense
+              </option>
+
+            </select>
           </label>
         </form>
       </div>
