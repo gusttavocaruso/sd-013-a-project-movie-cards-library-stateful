0e251b75b070f5e87ebc3d097b3d99cd40912c4e
V. Dionysio <vini.dionysio@gmail.com>
2021-07-27 20:17:20 -0300

    reqs 16, 17 and 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,19 +1,63 @@
 // implement MovieLibrary component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  onSearchTextChange({ target }) {
+    const { value } = target;
+    this.setState({
+      searchText: value,
+    });
+  }
+
+  onBookmarkedChange({ target }) {
+    const { value } = target;
+    this.setState({
+      bookmarkedOnly: value,
+    });
+  }
+
+  onSelectedGenreChange({ target }) {
+    const { value } = target;
+    this.setState({
+      selectedGenre: value,
+    });
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
         <SearchBar
-          searchText="uma string"
-          onSearchTextChange="uma callback"
-          bookmarkedOnly="um boolean"
-          onBookmarkedChange="uma callback"
-          selectedGenre="uma string"
-          onSelectedGenreChange="uma callback"
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList
+          movies={ movies }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          searchText={ searchText }
         />
         <AddMovie
           onClick="uma callback"
@@ -23,4 +67,7 @@ class MovieLibrary extends Component {
   }
 }
 
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf({}).isRequired,
+};
 export default MovieLibrary;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
+    const { movies, bookmarkedOnly, selectedGenre, searchText } = this.props;
+    let arrayMovies;
+    arrayMovies = movies
+      .map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
+    if (bookmarkedOnly) {
+      arrayMovies = movies
+        .filter((movie) => movie.bookmarked)
+        .map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
+    }
+
+    if (selectedGenre !== '') {
+      arrayMovies = movies
+        .filter((movie) => movie.genre === selectedGenre)
+        .map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
+    }
+
+    if (searchText !== '') {
+      arrayMovies = movies
+        .filter((movie) => {
+          const exp1 = movie.subtitle.toLowerCase().includes(searchText.toLowerCase());
+          const exp2 = movie.storyline.toLowerCase().includes(searchText.toLowerCase());
+          const exp3 = movie.title.toLowerCase().includes(searchText.toLowerCase());
+          if (!exp1 && !exp2 && !exp3) return null;
+          return movie;
+        })
+        .map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
+    }
 
     return (
       <div data-testid="movie-list" className="movie-list">
-        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        { arrayMovies }
       </div>
     );
   }
@@ -19,6 +44,9 @@ MovieList.propTypes = {
   movies: PropTypes.arrayOf(
     PropTypes.object,
   ).isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  searchText: PropTypes.string.isRequired,
 };
 
 export default MovieList;
