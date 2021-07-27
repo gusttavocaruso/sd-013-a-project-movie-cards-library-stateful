f531ddac29c7bd20cb8819a395ea65fe2b0fa5b0
Leonardo Santos <lcds90@gmail.com>
2021-07-27 18:33:54 -0300

    req 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,15 +12,33 @@ export class SearchBar extends Component {
     } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" htmlFor="searchText">
+        <label
+          data-testid="text-input-label"
+          htmlFor="searchText"
+        >
           Inclui o texto
           <input
-            type="text"
             data-testid="text-input"
+            type="text"
             name="searchText"
             id="searchText"
             value={ searchText }
             onChange={ onSearchTextChange }
+          />
+        </label>
+
+        <label
+          data-testid="checkbox-input-label"
+          htmlFor="bookmarkedOnly"
+        >
+          Mostrar somente favoritos
+          <input
+            data-testid="checkbox-input"
+            type="checkbox"
+            name="bookmarkedOnly"
+            id="bookmarkedOnly"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
           />
         </label>
       </form>
