d24d28f4b7ab181ab38414e7e8d0a93ad4e32f1a
Julia Baptista <baptista.julia@yahoo.com.br>
2021-07-27 18:09:08 -0300

    Quesito 1 e 2

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 
-import MovieList from './MovieList';
+// import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      // movies: [...this.props.movies],
+    };
   }
 
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={this.props.movies} />
-        <AddMovie />
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        {/* <MovieList movies={this.props.movies} />
+        <AddMovie /> */}
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,52 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label
+          htmlFor="search"
+          data-testid="text-input-label"
+        >
+          Inclui o texto:
+          <input
+            type="text"
+            name="search"
+            id="search"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <label
+          htmlFor="favorates"
+          data-testid="checkbox-input-label"
+        >
+          Mostrar somente favoritos:
+          <input
+            type="checkbox"
+            name="favorates"
+            id="favorates"
+            data-testid="text-input"
+            value={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.isRequired,
+};
+
+export default SearchBar;
