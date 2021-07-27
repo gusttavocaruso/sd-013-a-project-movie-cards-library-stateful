ee816d6c272966e016020b8ab609f6a4912587b1
ALINE <hoshinoaline@yahoo.com.br>
2021-07-27 14:49:05 -0300

    iniciando o projeto

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,14 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        jijijoi
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
