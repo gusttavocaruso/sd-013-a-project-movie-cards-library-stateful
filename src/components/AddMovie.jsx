b46fc09ee496acc6322477511339b79cccdcc9a6
JOSE VICTOR SANTIAGO FIGUEIREDO BAPTISTA <78240695+jvictorbap@users.noreply.github.com>
2021-07-27 18:24:37 -0300

    comitao bomba

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,17 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render() {
+    return (
+      <div>
+        <form data-testid="add-movie-form">
+          <label htmlFor="movieTitle" data-testid="title-input-label">
+            <input type="text" name="movieTitle" data-testid="title-input" />
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,31 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div className="search-bar">
+        <form data-testid="search-bar-form">
+          <label htmlFor="searchText" data-testid="text-input-label">
+            Inclui o texto
+            <input type="text" name="searchText" data-testid="text-input" />
+          </label>
+          <label htmlFor="favCheckBox" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input type="checkbox" name="favCheckBox" data-testid="checkbox-input" />
+          </label>
+          <label htmlFor="selectGender" data-testid="select-input-label">
+            Filtrar por gênero
+            <select name="selectGender" data-testid="select-input">
+              <option value="" data-testid="select-option">Todos</option>
+              <option value="action" data-testid="select-option">Ação</option>
+              <option value="comedy" data-testid="select-option">Comédia</option>
+              <option value="thriller" data-testid="select-option">Suspense</option>
+            </select>
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
