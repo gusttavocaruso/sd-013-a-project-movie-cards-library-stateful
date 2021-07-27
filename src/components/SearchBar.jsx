ea9d61c630c33bc851741a4eab4dcc013bd53c01
Fernando Costa <fgosuendacosta@gmail.com>
2021-07-27 13:52:24 -0300

    Adição requisitos 16

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -44,3 +44,24 @@ class SearchBar extends Component {
       </label>
     );
   }
+
+  renderSelect() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label data-testid="select-input-label" htmlFor="select-input">
+        Filtrar por gênero
+        <select
+          value={ selectedGenre }
+          onChange={ onSelectedGenreChange }
+          id="selectedGenre"
+          name="selectedGenre"
+          data-testid="select-input"
+        >
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
