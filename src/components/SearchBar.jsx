b717dc6d2d32269b499cff588aeac10665c248cd
sonkai <mfilho86@gmail.com>
2021-07-27 19:18:30 -0300

    passo 1 ok SearchBar e props na medidad do possivel do lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,25 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    // const {
+    //   searchText,
+    //   onSearchTextChange,
+    //   bookmarkedOnly,
+    //   selectedGenre,
+    //   onSelectedGenreChange,
+    // } = this.props;
+    return (
+      <p>Procure aqui!</p>
+    );
+  }
+}
+
+export default SearchBar;
