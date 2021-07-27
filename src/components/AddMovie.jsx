72093e585e67cad4ec756e12e67278384c362352
thomasravache <thomasravache31@gmail.com>
2021-07-27 20:58:27 -0300

    Crie um componente chamado <AddMovie />

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,29 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+
+  render() {
+    const { onClick } = this.props;
+
+    return ('');
+  }
+}
+
+AddMovie.PropTypes = {
+  onClick: PropTypes.func,
+};
+
+export default AddMovie;
