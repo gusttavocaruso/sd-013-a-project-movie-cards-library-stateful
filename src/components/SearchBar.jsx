1ff99fe20ae26168a0405d299680c532c2703f80
Fernando Costa <fgosuendacosta@gmail.com>
2021-07-27 13:51:28 -0300

    Adição requisitos 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../SearchBar.css';
+
+class SearchBar extends Component {
+  constructor() {
+    super();
+
+    this.renderTextInput = this.renderTextInput.bind(this);
+    this.renderCheckBox = this.renderCheckBox.bind(this);
+    this.renderSelect = this.renderSelect.bind(this);
+  }
