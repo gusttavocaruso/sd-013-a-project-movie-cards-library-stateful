538132612a2adde867c25c6c57caa5a4d2bddc2d
Luiza Antiques <luiza.antiques@gmail.com>
2021-07-27 18:17:37 -0300

    Conclui requisito 5 e corrige erros do lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,30 +12,42 @@ class SearchBar extends React.Component {
       onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label
-          htmlFor="search-text"
-          data-testid="text-input-label">
+        <label htmlFor="search-text" data-testid="text-input-label">
           Inclui o texto:
           <input
             type="text"
             id="search-text"
             value={ searchText }
             onChange={ onSearchTextChange }
-            data-testid="text-input"/>
+            data-testid="text-input"
+          />
         </label>
-        <label
-          htmlFor="favorite-check"
-          data-testid="checkbox-input-label">
+        <label htmlFor="favorite-check" data-testid="checkbox-input-label">
           Mostrar somente favoritos
           <input
             type="checkbox"
             id="favorite-check"
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
-            data-testid="checkbox-input"/>
+            data-testid="checkbox-input"
+          />
+        </label>
+        <label htmlFor="select-genre" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="select-genre"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
-    )
+    );
   }
 }
 
