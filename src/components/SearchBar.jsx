960f402944938456118e854c804dc929f13b4ec8
Leonardo Santos <lcds90@gmail.com>
2021-07-27 20:47:50 -0300

    component searchBarInput Dynamic

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { InputSearchBar } from './SearchBar_Input';
 
 export class SearchBar extends Component {
   render() {
@@ -12,53 +13,42 @@ export class SearchBar extends Component {
     } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" htmlFor="searchText">
-          Inclui o texto
-          <input
-            data-testid="text-input"
-            type="text"
-            name="searchText"
-            id="searchText"
-            value={ searchText }
-            onChange={ onSearchTextChange }
-          />
-        </label>
-
-        <label data-testid="checkbox-input-label" htmlFor="bookmarkedOnly">
-          Mostrar somente favoritos
-          <input
-            data-testid="checkbox-input"
-            type="checkbox"
-            name="bookmarkedOnly"
-            id="bookmarkedOnly"
-            checked={ bookmarkedOnly }
-            onChange={ onBookmarkedChange }
-          />
-        </label>
-
-        <label data-testid="select-input-label" htmlFor="selectedGenre">
-          Filtrar por gênero
-          <select
-            data-testid="select-input"
-            name="selectedGenre"
-            id="selectedGenre"
-            value={ selectedGenre }
-            onChange={ onSelectedGenreChange }
-          >
-            <option data-testid="select-option" value="">
-              Todos
-            </option>
-            <option data-testid="select-option" value="action">
-              Ação
-            </option>
-            <option data-testid="select-option" value="comedy">
-              Comédia
-            </option>
-            <option data-testid="select-option" value="thriller">
-              Suspense
-            </option>
-          </select>
-        </label>
+        <InputSearchBar
+          type="text"
+          information={ {
+            testId: { input: 'text-input', label: 'text-input-label' },
+            inputName: 'searchText',
+            textLabel: 'Inclui o texto',
+            value: searchText,
+            onChangeFunction: onSearchTextChange,
+          } }
+        />
+        <InputSearchBar
+          type="checkbox"
+          information={ {
+            testId: { input: 'checkbox-input', label: 'checkbox-input-label' },
+            inputName: 'bookmarkedOnly',
+            textLabel: 'Mostrar somente favoritos',
+            value: bookmarkedOnly,
+            onChangeFunction: onBookmarkedChange,
+          } }
+        />
+        <InputSearchBar
+          type="select"
+          information={ {
+            testId: { input: 'select-input', label: 'select-input-label' },
+            inputName: 'selectedGenre',
+            textLabel: 'Filtrar por gênero',
+            value: selectedGenre,
+            onChangeFunction: onSelectedGenreChange,
+            options: [
+              { value: '', text: 'Todos' },
+              { value: 'action', text: 'Ação' },
+              { value: 'comedy', text: 'Comédia' },
+              { value: 'thriller', text: 'Suspense' },
+            ],
+          } }
+        />
       </form>
     );
   }
