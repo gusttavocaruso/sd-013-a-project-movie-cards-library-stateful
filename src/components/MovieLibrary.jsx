21fcaec1750043158695c4c2deef20379e069c29
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2021-07-27 15:13:29 -0300

    Req 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,7 +5,10 @@ class MovieLibrary extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
     };
   }
 
