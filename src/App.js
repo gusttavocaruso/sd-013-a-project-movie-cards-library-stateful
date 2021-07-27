5f7ddddf3bacb1bc8e9d14403676063d1989b70e
Yuri Garrido <y.garrido.yuri@gmail.com>
2021-07-27 15:47:12 -0300

    renderiza input de pesquisa

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from './components/Header';
 import './App.css';
 import MovieList from './components/MovieList';
 import movies from './data';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
       <MovieList movies={ movies } />
     </div>
   );
