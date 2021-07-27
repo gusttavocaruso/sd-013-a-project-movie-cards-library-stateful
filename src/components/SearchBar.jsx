35811052c658cf9adf0f4900ad7733f077c32ade
Yuri Garrido <y.garrido.yuri@gmail.com>
2021-07-27 15:42:40 -0300

    renderiza os cards

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,19 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor="input-filter">
+            pesquise:
+            <input type="text" id="input-filter" />
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
