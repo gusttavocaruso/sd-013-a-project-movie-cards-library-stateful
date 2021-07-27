f67cce44a21056721ca0dc6982d81698652d26a2
Luiza Antiques <luiza.antiques@gmail.com>
2021-07-27 16:57:52 -0300

    Conclui requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
       selectedGenre,
       onSelectedGenreChange } = this.props;
     return (
-      <div></div>
+      <form data-testid="search-bar-form"></form>
     )
   }
 }
