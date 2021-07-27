5944a1d7ba2c45661d327b68f7023cfe50bcdccf
LuizFJP <luizfjportela@gmail.com>
2021-07-27 18:32:06 -0300

    requirement 03

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,16 @@ class SearchBar extends Component {
     const {searchText, onSearchTextchange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange} = this.props;
     return (
       <form data-testid="search-bar-form">
-        asd
+        <label htmlFor="search-text" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            id="search-text"
+            value={ searchText }
+            onChange={ onSearchTextchange }
+            data-testid="text-input"
+          />
+        </label>
       </form>
     );
   }
