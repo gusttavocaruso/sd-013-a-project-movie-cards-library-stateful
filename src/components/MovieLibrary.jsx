09b49c9c83479fcf4967ff78b7fb9bbc20483093
Gustavo Mauricio <gmauricio207@gmail.com>
2021-07-27 19:47:41 -0300

    Req 15 falta finalizar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,32 +1,28 @@
 import React, { Component } from 'react';
 import AddMovie from './AddMovie';
 import SearchBar from './SearchBar'
+import MovieList from './MovieList'
 
 export default class MovieLibrary extends Component {
   constructor() {
     super();
-    const { movies } = this.props;
-
+    
     this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      movies: movies,
+        searchText: '',
+        bookmarkedOnly: false,
+        selectedGenre: '',
+        movies: movies,
     };
-  }
+}
 
-  render() {
+render() {
+      const { movies } = this.props;
     return (
       <div>
-        <SearchBar 
-         searchText="uma string"
-         onSearchTextChange="uma callback"
-         bookmarkedOnly="um boolean"
-         onBookmarkedChange="uma callback"
-         selectedGenre="uma string"
-         onSelectedGenreChange="uma callback"
-         />
-        <AddMovie onClick="uma callback"/>
+          <MovieLibrary movies={movies} />
+          <SearchBar />
+          <AddMovie />
+          <MovieList />      
       </div>
 
     );
