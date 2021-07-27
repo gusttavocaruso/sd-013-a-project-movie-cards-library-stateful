605535923ed5fc96ba5a0ad83c6f7901f19f197d
sonkai <mfilho86@gmail.com>
2021-07-27 19:40:49 -0300

    passo 3 label de procura

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -9,19 +10,32 @@ class SearchBar extends React.Component {
   }
 
   render() {
-    // const {
-    //   searchText,
-    //   onSearchTextChange,
-    //   bookmarkedOnly,
-    //   selectedGenre,
-    //   onSelectedGenreChange,
-    // } = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      // bookmarkedOnly,
+      // selectedGenre,
+      // onSelectedGenreChange,
+    } = this.props;
     return (
       <form data-testid="search-bar-form">
-        O Forms aqui!
+        <label data-testid="text-input-label" htmlFor="searchText">
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
       </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: Proptypes.string.isRequired,
+  onSearchTextChange: Proptypes.func.isRequired,
+};
 
 export default SearchBar;
