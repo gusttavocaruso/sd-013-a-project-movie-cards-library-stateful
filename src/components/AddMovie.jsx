d75f2fb20f0aa3078209b2fd1f79bda52d992cf3
Juliane Marques <mqs.juliane@gmail.com>
2021-07-27 17:37:27 -0300

    add requisito 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -23,9 +23,19 @@ class AddMovie extends React.Component {
         <label data-testid="title-input-label">
           Título
           <input 
+            type="text"
             data-testid="title-input"
             onChange={  }
             name="title"
+          />
+        </label>
+        <label data-testid="subtitle-input-label">
+          Subtítulo
+          <input 
+            type="text"
+            data-testid="subtitle-input"
+            onChange={  }
+            name="subtitle"
           />
         </label>
       </form>
