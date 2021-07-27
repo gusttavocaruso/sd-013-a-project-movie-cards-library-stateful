6a4b98bf77ecef97a30cb58e029ca3cd49c62687
Juliane Marques <mqs.juliane@gmail.com>
2021-07-27 15:21:45 -0300

    add requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,10 @@
 // implement SearchBar component here
+
+class SearchBar extends React.Component {
+    render() {
+        const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+        return (
+            
+        )
+    }
+}
