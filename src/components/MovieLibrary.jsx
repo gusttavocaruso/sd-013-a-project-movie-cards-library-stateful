d9bf9e1b2c76435bfb58ad6a613800b542ebab3d
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-07-27 16:57:02 -0300

    cumpre requisitos 1 a 3

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,60 @@
 // implement MovieLibrary component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import movies from '../data';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      onSearchTextChange: () => null,
+      bookmarkedOnly: false,
+      onBookmarkedChange: () => null,
+      selectedGenre: '',
+      onSelectedGenreChange: () => null,
+    };
+  }
+
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.state;
+
+    return (
+      <>
+        <SearchBar
+          data={ {
+            searchText,
+            onSearchTextChange,
+            bookmarkedOnly,
+            onBookmarkedChange,
+            selectedGenre,
+            onSelectedGenreChange,
+          } }
+        />
+        <MovieList movies={ movies } />
+      </>
+    );
+  }
+}
+
+// MovieLibrary.propTypes = {
+//   movie: PropTypes.shape({
+//     title: PropTypes.string,
+//     subtitle: PropTypes.string,
+//     storyline: PropTypes.string,
+//     rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+//     imagePath: PropTypes.string,
+//   }).isRequired,
+// };
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,43 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+
 // implement SearchBar component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { data: {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } } = this.props;
+
+    console.log(searchText);
+    return (
+      <form action="#" data-testid="search-bar-form">
+        <label htmlFor="search" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            name="search"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+// SearchBar.propTypes = { SearchBar: PropTypes.number };
+
+SearchBar.defaultProps = {
+  SearchBar: 'undefined',
+};
+
+export default SearchBar;
