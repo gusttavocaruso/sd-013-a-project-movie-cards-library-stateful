6fcb8cef034519597c801362ef7302bdc1ae8531
V. Dionysio <vini.dionysio@gmail.com>
2021-07-27 15:43:48 -0300

    req 1 and 2

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,22 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar
+          searchText="uma string"
+          onSearchTextChange="uma callback"
+          bookmarkedOnly="um boolean"
+          onBookmarkedChange="uma callback"
+          selectedGenre="uma string"
+          onSelectedGenreChange="uma callback"
+        />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,14 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        SearchBar
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
