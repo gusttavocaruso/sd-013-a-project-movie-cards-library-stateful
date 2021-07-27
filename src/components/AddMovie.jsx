23ab90f933252e3b2286368b3bfc77292510440b
Demistocles <demi.treze@gmail.com>
2021-07-27 20:14:39 -0300

    requisitos 6, 7 e 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,46 @@
-// implement AddMovie component here
+import React from 'react';
+// import propTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    //  subtitle: '',
+      title: '',
+    //  imagePath: '',
+    //  storyline: '',
+    //  rating: 0,
+    //  genre: 'action',
+    };
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+  }
+
+  handleChangeTitle(event) {
+    this.setState({ title: event.target.value });
+  }
+
+  render() {
+    // const { onClick } = this.props;
+    const { title } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="input-title" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="input-title"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleChangeTitle }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  // onClick: propTypes.func.isRequired,
+};
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 
-// import MovieList from './MovieList';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
@@ -10,15 +11,20 @@ class MovieLibrary extends Component {
   // }
 
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        {/* <MovieList movies={this.props.movies} /> */}
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: propTypes.arrayOf.isRequired,
+};
 
 export default MovieLibrary;
