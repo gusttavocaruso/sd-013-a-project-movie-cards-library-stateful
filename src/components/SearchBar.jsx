5b054c9f228162e1e73cbaa89928fcf9ec565c03
Lanai Conceição <conceicao.lanai@gmail.com>
2021-07-27 19:43:51 -0300

    Requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,27 +2,36 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre, onSearchTextChange, onBookmarkedChange, onSelectedGenreChange } = this.props;
+    const {
+      searchText,
+      bookmarkedOnly,
+      selectedGenre,
+      onSearchTextChange,
+      onBookmarkedChange,
+      onSelectedGenreChange} = this.props;
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="xxx" data-testid="text-input-label">
           Inclui o texto:
           <input
+            type="text"
             id="xxx"
             value={ searchText }
             onChange={ onSearchTextChange }
             data-testid="text-input"
           />
         </label>
-        {/* { bookmarkedOnly }
-        { selectedGenre } */}
+        <label htmlFor="yyy" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            id="yyy"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
       </form>
-
-      // <div>
-      //   { searchText }
-      //   { bookmarkedOnly }
-      //   { selectedGenre }
-      // </div>
     );
   }
 }
