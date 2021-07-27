ee503cc7aa8647967d30ad716e0787757f5ae3d8
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2021-07-27 19:04:08 -0300

    Adiciona requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,54 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="subtitle">
+          Subtítulo
+          <input
+            name="subtitle"
+            type="text"
+          />
+        </label>
+        <label htmlFor="title">
+          Título
+          <input
+            name="title"
+            type="text"
+          />
+        </label>
+        <label htmlFor="imagem">
+          Caminho da Imagem
+          <input
+            name="imagem"
+            type="text"
+          />
+        </label>
+        <label htmlFor="sinopse">
+          Sinopse
+          <input
+            name="sinopse"
+            type="text"
+          />
+        </label>
+        <label htmlFor="avaliação">
+          Avaliação
+          <input
+            name="avaliação"
+            type="text"
+          />
+        </label>
+        <label htmlFor="gênero">
+          Genêro
+          <input
+            name="gênero"
+            type="text"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
