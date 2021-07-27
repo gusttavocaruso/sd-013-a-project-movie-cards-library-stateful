4098c6acb6a7cce0f5a03066bd3013369e05ba0b
Gustavo Pia <piagja@gmail.com>
2021-07-27 18:33:13 -0300

    Realizou os requisitos 1 ao 5 - criando o searchBar

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,25 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class AddMovie extends Component {
+  // constructor() {
+  //   super();
+
+  //   this.state = {
+  //     substitle: '',
+  //     title: '',
+  //     imagePath: '',
+  //     storyline: '',
+  //     rating: 0,
+  //     genre: ''
+  //   };
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1>AddMovie</h1>
+      </div>
+    );
+  }
+}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,67 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+        <h1>Filtros Avançados</h1>
+        {/* aqui é um componente */}
+        <label
+          htmlFor="search-term"
+          data-testid="text-input-label"
+        >
+          Inclui o texto:
+          <input
+            type="text"
+            name="search-text"
+            id="search-term"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+        {/* aqui é um componente */}
+        <label
+          data-testid="checkbox-input-label"
+          htmlFor="bookmark"
+        >
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            name="bookmark"
+            id="bookmark"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
+        {/* aqui é um componente */}
+        <label htmlFor="genres" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
