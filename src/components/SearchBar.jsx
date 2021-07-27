15e1271d99f5aee004b8a8da125cbd7c50d52a63
ElaineMoreira <seuemail@exemplo.br>
2021-07-27 18:09:14 -0300

    Adiciona requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,24 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
-    render() {
-        const { searchText, onSearchTextChange, bookmarkedOnly, 
-            onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
-        return (
-            <form data-testid="search-bar-form">
-                
-            </form>
-        )
-    }
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="text">
+          Inclui o texto:
+          <input
+            value="searchText"
+            onChange="onSearchTextChange"
+            data-testid="text-input"
+            id="text"
+          />
+        </label>
+      </form>
+    );
+  }
 }
 
 SearchBar.propTypes = {
-    searchText: PropTypes.string,
-    onSearchTextChange: PropTypes.func,
-    bookmarkedOnly: PropTypes.bool,            
-    onBookmarkedChange: PropTypes.func, 
-    selectedGenre: PropTypes.string,
-    onSelectedGenreChange: PropTypes.func,
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
 }.isRequired;
 
 export default SearchBar;
