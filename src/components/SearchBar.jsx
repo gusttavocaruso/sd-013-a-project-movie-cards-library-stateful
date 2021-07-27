16095995ed68f67149338becc388dadce490c4a4
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-07-27 17:51:10 -0300

    Retira alguns erros de Lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
-  render(){
-    const { searchText, onSearchTextChange , bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
-    return(
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
+    return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" htmlFor="">
-        Inclui o texto:
-          <input data-testid="text-input" nametype="text" value={searchText} onChange={onSearchTextChange}/>
+        <label data-testid="text-input-label" htmlFor="text-input">
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            nametype="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
         </label>
       </form>
-    )
+    );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+SearchBar.defaultProps = {
+  bookmarkedOnly: false,
+};
 
 export default SearchBar;
