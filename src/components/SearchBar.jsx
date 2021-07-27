7af7259725db61c199e34817a6a70dbadd50da90
Lanai Conceição <conceicao.lanai@gmail.com>
2021-07-27 18:13:46 -0300

    Requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,17 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.props;
+    return (
+      <div>
+        Teste
+        { searchText }
+        { bookmarkedOnly }
+        { selectedGenre }
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
