e47e31d71c66f1985312dd37556c4d8f59c22044
ElaineMoreira <seuemail@exemplo.br>
2021-07-27 17:44:55 -0300

    Adiciona requisito 1

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,5 @@
-// implement MovieLibrary component here.
+import react from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+import PropTypes from 'prop-types';
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,22 @@
-// implement SearchBar component here.
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+    render() {
+        const { searchText, onSearchTextChange, bookmarkedOnly, 
+            onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+        return (
+        )
+    }
+}
+
+SearchBar.propTypes = {
+    searchText: PropTypes.string,
+    onSearchTextChange: PropTypes.func,
+    bookmarkedOnly: PropTypes.bool,            
+    onBookmarkedChange: PropTypes.func, 
+    selectedGenre: PropTypes.string,
+    onSelectedGenreChange: PropTypes.func,
+}.isRequired;
+
+export default SearchBar;
