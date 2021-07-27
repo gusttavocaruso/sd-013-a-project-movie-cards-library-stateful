c931d6b5b23d1b4750aff90932822f020870ebb5
ArthurMGoulart <budiarthur@gmail.com>
2021-07-27 15:35:49 -0300

    Realizando requisitos do componente SearchBar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,36 @@
 // implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: '',
+      selectedGenre: '',
+    }
+  }
+  onSearchTextChange = () => {
+
+  }
+  onBookmarkedChange = () => {
+
+  }
+  onSelectedGenreChange = () => {
+
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar searchText={ this.state.searchText } onSearchTextChange={ this.onSearchTextChange } 
+        bookmarkedOnly={ this.state.bookmarkedOnly } onBookmarkedChange={ this.onBookmarkedChange }
+        selectedGenre={ this.state.selectedGenre } onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+      </div>
+    )
+  }
+}
+
+export default MovieLibrary;
