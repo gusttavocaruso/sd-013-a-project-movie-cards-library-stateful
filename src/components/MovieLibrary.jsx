367440d02f67a5c4b938672bdf2500d536cca9fc
Fernando Costa <fgosuendacosta@gmail.com>
2021-07-27 13:44:19 -0300

    Adição requisitos 6 e 7

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,7 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
