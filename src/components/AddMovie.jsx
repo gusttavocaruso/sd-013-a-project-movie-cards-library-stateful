3cee49ea3ee24d0fc742944c8c9b7a83516b7a76
Breno Lopes <brenokos4@gmail.com>
2021-07-27 17:17:28 -0300

    add requisitos 6, 7 e 8!

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,48 @@
 // implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types'
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="input-text2">
+          Título
+          <input
+            type="text"
+            id="input-text2"
+            data-testid="title-input"
+            onChange={ this.handleChange }
+            name="title"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
