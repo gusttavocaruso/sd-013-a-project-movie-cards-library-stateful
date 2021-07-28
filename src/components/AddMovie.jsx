405cfbe830da0afba9d7d5e2f33df2b553b530f6
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-07-28 01:42:37 -0300

    Requisito 6 - ok

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,23 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <h1 />
+    );
+  }
+}
