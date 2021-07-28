1d083cbbbe9f34ae4def7b9a8915fd8be691d0a4
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-07-28 13:23:32 -0300

    Requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,6 +5,16 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: [],
+    };
+  }
+
   render() {
     const { movies } = this.props;
     return (
