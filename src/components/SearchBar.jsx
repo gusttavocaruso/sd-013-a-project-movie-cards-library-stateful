bcc9f4df2962625d1089589c36a61363b695316c
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2021-07-28 04:31:26 -0300

    added Notice regarding filter limitation and update CSS

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,51 +4,52 @@ import PropTypes from 'prop-types';
 class SearchBar extends Component {
   render() {
     const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = this.props;
-
+      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre,
+      onSelectedGenreChange } = this.props;
     return (
-      <form data-testid="search-bar-form" className="search-bar">
-        <label data-testid="text-input-label" htmlFor="searchText">
-          Inclui o texto: &nbsp;
-          <input
-            data-testid="text-input"
-            type="text"
-            name="searchText"
-            value={ searchText }
-            onChange={ onSearchTextChange }
-          />
-        </label>
-        <label data-testid="checkbox-input-label" htmlFor="bookmarked">
-          Mostrar somente favoritos: &nbsp;
-          <input
-            data-testid="checkbox-input"
-            type="checkbox"
-            name="bookmarkedOnly"
-            checked={ bookmarkedOnly }
-            onChange={ onBookmarkedChange }
-          />
-        </label>
-        <label data-testid="select-input-label" htmlFor="genre">
-          Filtrar por gênero: &nbsp;
-          <select
-            data-testid="select-input"
-            name="selectedGenre"
-            value={ selectedGenre }
-            onChange={ onSelectedGenreChange }
-          >
-            <option data-testid="select-option" value="">Todos</option>
-            <option data-testid="select-option" value="action">Ação</option>
-            <option data-testid="select-option" value="comedy">Comédia</option>
-            <option data-testid="select-option" value="thriller">Suspense</option>
-          </select>
-        </label>
-      </form>
+      <div className="search-bar-container">
+        <span className="filter-warning">
+          Notice: Due to current technical limitations,
+          only one filter can be applied at a time.
+          This is a work in progress, we apologize for the inconvenience.
+        </span>
+        <form data-testid="search-bar-form" className="search-bar">
+          <label data-testid="text-input-label" htmlFor="searchText">
+            Inclui o texto: &nbsp;
+            <input
+              data-testid="text-input"
+              type="text"
+              name="searchText"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+            />
+          </label>
+          <label data-testid="checkbox-input-label" htmlFor="bookmarked">
+            Mostrar somente favoritos: &nbsp;
+            <input
+              data-testid="checkbox-input"
+              type="checkbox"
+              name="bookmarkedOnly"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+            />
+          </label>
+          <label data-testid="select-input-label" htmlFor="genre">
+            Filtrar por gênero: &nbsp;
+            <select
+              data-testid="select-input"
+              name="selectedGenre"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+            >
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
+          </label>
+        </form>
+      </div>
     );
   }
 }
