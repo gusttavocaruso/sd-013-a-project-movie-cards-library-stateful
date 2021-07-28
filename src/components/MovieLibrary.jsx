85642cb3f38cff904cd7d7f22614ac71e9fab1d6
Pedro Muniz <pedro.muniz@aluno.uece.br>
2021-07-28 16:17:01 -0300

    req 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,30 +1,53 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import MovieList from './MovieList';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  /* constructor(props) {
+  constructor(props) {
     super(props);
-  } */
+
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
 
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        {/* <MovieList movies={ this.props.movies } /> */}
+        <SearchBar 
+          searchText= { searchText }
+          onSearchTextChange= { this.handleChange }
+          bookmarkedOnly= { bookmarkedOnly }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleChange }
+        />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
 
-/* MovieLibrary.propTypes = {
+MovieLibrary.propTypes = {
   movies: PropTypes.arrayOf(
     PropTypes.object,
   ).isRequired,
-}; */
+};
 
 export default MovieLibrary;
