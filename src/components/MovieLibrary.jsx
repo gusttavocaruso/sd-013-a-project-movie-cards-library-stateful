8012cad75e74548c60fd0f27cdda812b0dce6969
Rafael Victor <rafavictorgs@gmail.com>
2021-07-28 16:17:00 -0300

    Resolve requisito 3

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import data from '../data';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: data,
+    };
+  }
+
+  onSearchTextChange = (event) => {
+    this.setState({
+      searchText: event.target.value,
+    });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <SearchBar />
     );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
-      <form data-testid="search-bar-form" />
+      <form data-testid="search-bar-form">
+        <label htmlFor="input-text" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            id="input-text"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            placeholder="Pesquisar filme..."
+          />
+        </label>
+      </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
+
+// SearchBar.defaultProps = {
+//   // searchText: '',
+//   // onSearchTextChange: () => {},
+// };
 
 export default SearchBar;
