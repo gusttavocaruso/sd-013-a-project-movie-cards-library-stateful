bf04e5f56a8772607037b535c2a3b765ec404059
Isabella Antunes <bella.antunes2@gmail.com>
2021-07-28 16:02:55 -0300

    Requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
   render() {
     return (
-      <div>
-        Hello World!
-      </div>
+      <form data-testid="add-movie-form">
+        
+      </form>
     );
   }
 }
