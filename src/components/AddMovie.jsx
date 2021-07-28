caa2584b47280a448c38903a2b414de2c7f52e2b
ALINE <hoshinoaline@yahoo.com.br>
2021-07-28 11:07:27 -0300

    requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,10 +2,40 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        dqdwq
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="title"
+            id="title"
+            data-testid="title-input"
+            title={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
       </form>
     );
   }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
@@ -43,6 +44,7 @@ class MovieLibrary extends React.Component {
           onSelectedGenreChange={ this.handleChange }
         />
         <MovieList movies={ movies } />
+        <AddMovie />
       </div>
     );
   }
