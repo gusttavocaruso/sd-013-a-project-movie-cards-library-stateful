65a3db3836229b48501eca79c87ccbe66d369028
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2021-07-28 15:46:28 -0300

    Resolve requisitos 6 ao 14

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,80 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MoviesInput from './MoviesInput';
+import NewMoviesInput from './NewMoviesInput';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.changeStates = this.changeStates.bind(this);
+    this.clickButton = this.clickButton.bind(this);
+  }
+
+  changeStates(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  clickButton() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  render() {
+    const {
+      subtitle,
+      title,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.state;
+
+    return (
+      <form data-testid="add-movie-form">
+        <MoviesInput
+          title={ title }
+          subtitle={ subtitle }
+          imagePath={ imagePath }
+          storyline={ storyline }
+          changeStates={ this.changeStates }
+        />
+        <NewMoviesInput
+          storyline={ storyline }
+          rating={ rating }
+          genre={ genre }
+          changeStates={ this.changeStates }
+        />
+        <button data-testid="send-button" onClick={ this.clickButton } type="button">
+          Adicionar filme
+        </button>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
---
+++ src/components/NewMoviesInput.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class NewMoviesInput extends React.Component {
+  render() {
+    const { storyline, rating, genre, changeStates } = this.props;
+
+    return (
+      <div>
+        <label htmlFor="story-input" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            name="storyline"
+            id="story-input"
+            cols="30"
+            rows="10"
+            value={ storyline }
+            data-testid="storyline-input"
+            onChange={ changeStates }
+          />
+        </label>
+        <label htmlFor="rating-input" data-testid="rating-input-label">
+          Avaliação
+          <input
+            type="number"
+            name="rating"
+            id="rating-input"
+            value={ rating }
+            data-testid="rating-input"
+            onChange={ changeStates }
+          />
+        </label>
+        <label htmlFor="genre-select" data-testid="genre-input-label">
+          Gênero
+          <select
+            name="genre"
+            id="genre-select"
+            value={ genre }
+            data-testid="genre-input"
+            onChange={ changeStates }
+          >
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
+      </div>
+    );
+  }
+}
+
+NewMoviesInput.propTypes = {
+  storyline: PropTypes.string,
+  rating: PropTypes.number,
+  genre: PropTypes.string,
+  changeStates: PropTypes.func.isRequired,
+};
+
+NewMoviesInput.defaultProps = {
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
+
+export default NewMoviesInput;
