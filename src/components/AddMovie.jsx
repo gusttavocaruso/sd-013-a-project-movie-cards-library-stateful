b5e587908898c3b1c47485c3b3d8fdd7cb5925eb
ElaineMoreira <seuemail@exemplo.br>
2021-07-28 10:17:57 -0300

    Adiciona requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,39 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const stateInitial = {
-        title: '',
-        subtitle: '',
-        imagePath: '',
-        storyline: '',
-        rating: 0,
-        genre: 'action',
-    };
+  title: '',
+  subtitle: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
 
 class AddMovie extends React.Component {
-    constructor() {
-        super();
-        this.state = stateInitial;
-        this.handleChange = this.handleChange.bind(this); // 1
-        this.handleClick = this.handleClick.bind(this); // 2
+  constructor() {
+    super();
+    this.state = stateInitial;
+    this.handleChange = this.handleChange.bind(this); // 1
+    this.handleClick = this.handleClick.bind(this); // 2
+  }
+
+  handleChange({ target }) { // 1
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  handleClick() { // 2
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState(stateInitial);
+  }
+
+  render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+          
+      </form>
+    );
+  }
 }
-
-    handleChange({ target }) { // 1
-        this.setState({
-            [target.name]: target.value,
-        });
-    }
-
-    handleClick() { // 2
-        const { onClick } = this. props;
-        onClick(this.state);
-        this.setState(stateInitial); 
-    }
-
-    render() {
-        const { title, subtitle,  imagePath, storyline, rating, genre } = this.state;
-        return (
-        )
-    }
-};
 
 export default AddMovie;
