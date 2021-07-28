bbc621e7e7933a63d2ba36d9a7373b2ab88abfef
Lanai Conceição <conceicao.lanai@gmail.com>
2021-07-28 13:51:55 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,29 @@
-// implement AddMovie component here
+import { tsConstructorType } from '@babel/types';
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+
+  // function aqui
+
+  render() {
+    return (
+      <div>
+        <p>Teste</p>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
