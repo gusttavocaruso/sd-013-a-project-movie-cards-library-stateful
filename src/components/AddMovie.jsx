a6aa8d2572ff3bbd8cabae4652c2a33c4cb47967
Icaro Ferreira <icaro.iky@gmail.com>
2021-07-28 15:20:09 -0300

    Add label and input to title and subtitle on AddMovie component

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -19,7 +19,22 @@ class AddMovie extends React.Component {
     return (
       <div>
         <form data-testid="add-movie-form">
-          Hello Form
+          <label htmlFor="inputTitle" data-testid="title-input-label">Título
+            <input
+              data-testid="title-input"
+              type="text"
+              id="inputTitle"
+              name="inputTitle"
+            />
+          </label>
+          <label htmlFor="inputSubtitle" data-testid="subtitle-input-label">Subtítulo
+            <input
+              data-testid="subtitle-input"
+              type="text"
+              id="inputSubtitle"
+              name="inputSubtitle"
+          />
+          </label>
         </form>
       </div>
     );
