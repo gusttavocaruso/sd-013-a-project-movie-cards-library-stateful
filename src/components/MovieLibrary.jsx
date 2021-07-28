3d66360513bab40d7ad5153e925cb8e408d52d53
sonkai <mfilho86@gmail.com>
2021-07-28 16:54:08 -0300

    req 15, agora começa a ficar dificil

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,24 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { movies } = this.props;
+    return (
+      <p>{ movies }</p>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
+
+export default MovieLibrary;
