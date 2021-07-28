511d3596404dd58588e5d0d615ed25c869f3811f
Luiza Antiques <luiza.antiques@gmail.com>
2021-07-28 12:43:33 -0300

    Conclui requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,29 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  callback: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
