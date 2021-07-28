0e1b9e5d9db1c5b2c2700aebf9aa05c0b4a27550
Luiza Antiques <luiza.antiques@gmail.com>
2021-07-28 13:05:32 -0300

    Conclui requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -13,11 +13,41 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.changeTitle = this.changeTitle.bind(this);
+  }
+
+  changeTitle(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
+    const { onClick } = this.props;
+    const {
+      subtitle,
+      title,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.state;
+
     return (
       <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="title"
+            id="title-input"
+            data-testid="title-input"
+            onChange={ this.changeTitle }
+            value={ title }
+          />
+        </label>
       </form>
     );
   }
