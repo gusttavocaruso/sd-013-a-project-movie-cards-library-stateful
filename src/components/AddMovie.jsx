951f2e23b2da85373937ecd6b847bf5b26fea7c6
sonkai <mfilho86@gmail.com>
2021-07-27 21:25:45 -0300

    passo seisja o que deus quiser?

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// boooooora!
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // subtitle: '',
+      // title: '',
+      // imagePath: '',
+      // storyLine: '',
+      // rating: 0,
+      // genre: '',
+    };
+  }
+
+  render() {
+    return (
+      <p>Add movie here</p>
+    );
+  }
+}
+
+export default AddMovie;
