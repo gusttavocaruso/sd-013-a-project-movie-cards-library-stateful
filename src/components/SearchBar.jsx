015f66723724e255a2708397781a6b6f4a2bc781
LuizFJP <luizfjportela@gmail.com>
2021-07-27 22:46:16 -0300

    requirement 03 fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    const {searchText, onSearchTextchange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange} = this.props;
+    const {searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange} = this.props;
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="search-text" data-testid="text-input-label">
@@ -11,8 +11,18 @@ class SearchBar extends Component {
             type="text"
             id="search-text"
             value={ searchText }
-            onChange={ onSearchTextchange }
+            onChange={ onSearchTextChange }
             data-testid="text-input"
+          />
+        </label>
+        <label htmlFor="favorite" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checked"
+            id="favorite"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
           />
         </label>
       </form>
