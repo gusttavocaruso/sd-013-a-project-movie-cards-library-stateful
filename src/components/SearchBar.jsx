13eee502936598f3254a622da1f07f2d0b1d52ec
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-07-28 01:07:25 -0300

    Requisito 4 e 5 - ok

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,27 +3,46 @@ import React from 'react';
 export default class extends React.Component {
   render() {
     const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = this.props;
+      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <section>
         <form action="" data-testid="search-bar-form">
-          <label htmlFor="" data-testid="text-input-label">
+          <label htmlFor="text-input" data-testid="text-input-label">
             Inclui o texto:
             <input
               type="text"
-              name=""
-              id=""
               data-testid="text-input"
               value={ searchText }
               onChange={ onSearchTextChange }
             />
           </label>
+
+          <label htmlFor="#" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              value={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+              data-testid="checkbox-input"
+              checked
+            />
+          </label>
+
+          <label htmlFor="#" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              data-testid="select-input"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+            >
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
+          </label>
+
         </form>
       </section>
     );
