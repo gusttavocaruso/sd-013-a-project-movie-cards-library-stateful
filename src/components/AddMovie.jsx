bf229d55125ea7271978e9ce26f1aa8dcc2ace39
Isabella Antunes <bella.antunes2@gmail.com>
2021-07-28 18:33:39 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 class AddMovie extends React.Component {
   constructor() {
     super();
+    this.handleChange = this.handleChange.bind(this);
+
     this.state = {
       subtitle: '',
       title: '',
@@ -13,10 +15,28 @@ class AddMovie extends React.Component {
     };
   }
 
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { subtitle, title, imagePath, storyLine, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            id="title-input"
+            type="text"
+            data-testid="title-input"
+            value={ title }
+            name="title"
+            onChange={ this.handleChange }
+          />
+        </label>
       </form>
     );
   }
