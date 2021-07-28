092aaf31b66c3f9a699f6852da34c30912df7981
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-07-28 01:47:10 -0300

    Requisito 8 - ok

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -18,7 +18,10 @@ export default class AddMovie extends React.Component {
     const { onClick } = this.props;
     return (
       <form action="" data-testid="add-movie-form">
-        
+        <label htmlFor="#" data-testid="title-input-label">
+          Título
+          <input type="text" value={ this.title } onChange={ onClick } data-testid="title-input" />
+        </label>
       </form>
     );
   }
