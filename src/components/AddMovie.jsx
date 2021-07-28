4129e958b458e559662b401eda3ea16580ba49e1
Juliane Marques <mqs.juliane@gmail.com>
2021-07-28 16:26:28 -0300

    add requisito refatorado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,11 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 class AddMovie extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       subtitle: '',
@@ -17,16 +18,26 @@ class AddMovie extends React.Component {
 
   }
 
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.arget.value,
+    });
+  }
+
   render() {
+  const { onCLick } = this.props;
+  const { subtitle, title, imagePath, storyLine, rating, genre } = this.state;
+
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label">
-          Título
+          Título:
           <input 
             type="text"
             data-testid="title-input"
-            onChange={  }
+            onChange={ this.handleChange }
             name="title"
+            value={ title }
           />
         </label>
         <label data-testid="subtitle-input-label">
@@ -34,11 +45,47 @@ class AddMovie extends React.Component {
           <input 
             type="text"
             data-testid="subtitle-input"
-            onChange={  }
+            onChange={ this.handleChange }
             name="subtitle"
+            value={ subtitle }
+          />
+        </label>
+        <label data-testid="image-input-label">
+          Imagem
+          <input 
+            value={ imagePath }
+            type=""
+            data-testid="image-input"
+            onChange={ this.handleChange }
+            name="imagePath"
+          />
+        </label>
+        <label data-testid="storyline-input-label">
+          Sinopse:
+          <textarea
+            name="storyLine"
+            value={ storyLine }
+            data-testid="storyline-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="rating-input-label">
+          Avaliação:
+          <input
+            name="rating"
+            type="number"
+            value={ rating }
+            data-testid="rating-input"
+            onChange={ this.handleChange }
           />
         </label>
       </form>
     )
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
