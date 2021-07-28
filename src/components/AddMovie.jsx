39f82ee1fab9a3fefc11dcd8fb861f2c27e2051c
Debora Teodorico <debora.steodorico@gmail.com>
2021-07-28 15:10:58 -0300

    Adicionando as questoes do 6 ao 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,69 @@
-// implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  render() {
+    const { title, subtitle, imagePath } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="text">
+          Título
+          <input
+            id="text"
+            type="text"
+            data-testid="title-input"
+            name="title"
+            title={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="subtitle-input-label" htmlFor="subtitle2">
+          Subtítulo
+          <input
+            id="subtitle2"
+            type="text"
+            name="subtitle"
+            data-testid="subtitle-input"
+            subtitle={ subtitle }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="image-input-label" htmlFor="imagePath">
+          Imagem
+          <input
+            id="imagePath"
+            type="text"
+            data-testid="image-input"
+            name="imagePath"
+            imagePath={ imagePath }
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+// AddMovie.propTypes = {
+//   onClick: PropTypes.func.isRequired,
+// };
+
+export default AddMovie;
