f0be15ba95420b4cea7cb706ce87cf6192a9389e
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-07-28 17:01:22 -0300

    add first requirement

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 
+import SearchBar from './components/SearchBar';
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar
+        searchText=""
+        onSearchTextChange="callback"
+        bookmarkedOnly="false"
+        onBookmarkedChange=""
+        onSelectedGenreChange="callback"
+      />
     </div>
   );
 }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,9 +4,10 @@ import PropTypes from 'prop-types';
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
+
     return (
       <div className="movie-card-rating" data-testid="rating">
-        <span className="rating">{rating}</span>
+        <span className="rating">{ rating }</span>
       </div>
     );
   }
