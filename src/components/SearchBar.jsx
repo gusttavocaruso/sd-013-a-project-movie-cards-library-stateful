c681c941203ff9c26416b9e7baabf4151d95bcc5
Adriana Barreto <adri92barreto123@gmail.com>
2021-07-28 18:10:28 -0300

    Inicia primeira questao

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,23 @@
-// implement SearchBar component here
+import React from 'react';
+import MovieList from './MovieList';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <MovieList />
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  movie: PropTypes.shape({
+    searchText: PropTypes.string,
+    onSearchTextChange: PropTypes.callback,
+    bookmarkedOnly: PropTypes.boolean,
+    onBookmarkedChange: PropTypes.callback,
+    selectedGenre: PropTypes.string,
+    onSelectedGenreChange: PropTypes.callback,
+  }).isRequired,
+};
+
+export default SearchBar;
