f89dba8cdc84a1818ed8944992432de23c9b22af
ALINE <hoshinoaline@yahoo.com.br>
2021-07-27 21:53:10 -0300

    requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,11 @@
-// implement SearchBar component here
+// fonte para chamar booleanos no proptype https://pt-br.reactjs.org/docs/typechecking-with-proptypes.html
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    const { searchText, onSearchTextChange } = this.props;
+    const { searchText, onSearchTextChange,
+      bookmarkedOnly, onBookmarkedChange } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="searchText" data-testid="text-input-label">
@@ -18,6 +19,17 @@ class SearchBar extends Component {
             onChange={ onSearchTextChange }
           />
         </label>
+        <label htmlFor="favoritos" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            name="bookMarkedOnly"
+            id="favoritos"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
       </form>
     );
   }
@@ -26,6 +38,8 @@ class SearchBar extends Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
