4f11bf5620db3841a6a0f2447e5200f7aa3e68d1
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-07-28 18:41:48 -0300

    6th requirement solved without lint issues yet

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from './components/Header';
 import './App.css';
 
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AddMovie />
       <SearchBar
         searchText="something"
         // onSearchTextChange="callback"
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,34 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const {
+      title, subtitle, imagePath, storyLine, rating, genre,
+    } = this.state;
+
+    return (
+      <span>
+        subtitulo,
+        titulo,
+        caminho da imagem,
+        sinopse,
+        avaliação,
+        gênero
+      </span>
+    );
+  }
+}
+
+export default AddMovie;
