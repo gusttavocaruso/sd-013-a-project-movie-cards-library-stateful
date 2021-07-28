0600c15517aeae9a0ae1201dafe7770673e9c3d1
icaroMelo <exemployt@gmail.com>
2021-07-28 13:18:51 -0300

    Lint

---
+++ src/components/Genre.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Genre extends React.Component {
+  render() {
+    const { genre, handleChange } = this.props;
+    return (
+      <label data-testid="genre-input-label" htmlFor="Genre">
+        Gênero
+        <select data-testid="genre-input" value={ genre } onChange={ handleChange }>
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+}
+
+Genre.propTypes = {
+  genre: PropTypes.number.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default Genre;
---
+++ src/components/Rate.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rate extends React.Component {
+  render() {
+    const { rating, handleChange } = this.props;
+    return (
+      <label data-testid="rating-input-label" htmlFor="rate">
+        Avaliação
+        <input
+          type="number"
+          data-testid="rating-input"
+          value={ rating }
+          onChange={ handleChange }
+        />
+      </label>
+    );
+  }
+}
+
+Rate.propTypes = {
+  rating: PropTypes.number.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default Rate;
