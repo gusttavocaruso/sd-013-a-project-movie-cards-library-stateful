f11d7cc117100e3b023ffa92fabb4e96047c5e02
ElaineMoreira <seuemail@exemplo.br>
2021-07-28 10:44:36 -0300

    Adiciona requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -34,7 +34,15 @@ class AddMovie extends React.Component {
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-          
+        <label data-testid="title-input-label" htmlFor="title">
+          Título
+          <input
+            value={ title }
+            data-testid="title-input"
+            id="title"
+            onChange={ this.handleChange }
+          />
+        </label>
       </form>
     );
   }
