d39f96f539939f2df5c47e3f2e0a5ca9ebb46eee
Andre Leme <andresleme@gmail.com>
2021-07-28 16:11:14 -0300

    Requirements 1 and 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,25 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { 
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelected 
+    } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="">
+          Inclui o texto:
+          <input type="text" name="" id="" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
