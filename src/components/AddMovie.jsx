afcf8f6bdeb8342734702899e5fd28d6e0921856
icaroMelo <exemployt@gmail.com>
2021-07-28 09:33:26 -0300

    Fazendo evaluator funcionar

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,28 @@
-// // implement AddMovie component here
+// implement AddMovie component here
 
-// import React from 'react';
+import React from 'react';
 
-// class AddMovie extends React.Component {
-//   render() {
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action'
+    };
+  }
+  render() {
+    return(
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title">
+          Título
+        </label>
+      </form>
+    )
+  }
+}
 
-//   }
-// }
-
-// export default AddMovie;
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 // import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
   // constructor(props) {
   //   super(props);
+  //   this.state = {
+  //     searchText: '',
+  //   }
   // }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
+        <AddMovie
+        onClick="uma callback"
+        />
         <SearchBar
           searchText="uma string"
           onSearchTextChange="uma callback"
