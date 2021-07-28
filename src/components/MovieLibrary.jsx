8ee6d5ae1dcb2399e9b28402e8472deba385bed2
Luiza Antiques <luiza.antiques@gmail.com>
2021-07-28 16:32:59 -0300

    Conclui requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class MovieLibrary extends React.Component {
-  render() {
+  constructor(props) {
+    super(props)
     const { movies } = this.props;
 
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: movies,
+    }
+  }
+  render() {
     return(
       <div />
     )
