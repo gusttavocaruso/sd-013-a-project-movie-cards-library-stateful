c8b08d12cc41ce5fa48ca71dcfed3ba4c2c91fc4
LuizFJP <luizfjportela@gmail.com>
2021-07-28 14:46:06 -0300

    requirement 08 and 09

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -17,7 +17,14 @@ class AddMovie extends Component {
     const onClick = this.props;
     return (
       <form data-testid="add-movie-form">
-        
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+        </label>
+        <input type="text" id="title" data-testid="title-input" onChange={ this.state.title } />
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          <input type="text" value={ this.state.subtitle } data-testid="subtitle-input" onChange={ this.state.subtitle } />
+        </label>
       </form>
     );
   }
