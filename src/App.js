17cfc802acec15b708011e6fa0931c09b7c14cf5
igor <igorhdsouza@gmail.com>
2021-07-28 17:54:22 -0300

    cria os componentes e renderiza dentro do componente pai

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import movies from './data';
 import Header from './components/Header';
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <MovieLibrary movies
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,17 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+    constructor() {
+        super();
+        
+    }
+    
+    render() {
+        return (
+            <SearchBar />
+            <MovieList />
+            <AddMovie />
+        );
+    };
+}
