d142cc58ab2bd23ba3e11535c062461bb54c9379
ElaineMoreira <seuemail@exemplo.br>
2021-07-28 16:12:02 -0300

    Agora sim...Adiciona requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,15 +2,25 @@ import react from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
-// import MovieList from './MovieList';.
+import MovieList from './MovieList';
 
 
 class MovieLibrary extends React.Component {
     constructor(props) {
-    }
+    
     super(props);
 
+    const { movies } = this.props;
+
+    this.state = {
+        searchText: '',
+        bookmarkedOnly: false,
+        selectedGenre:'',
+        movies,
+    }
+}
     render() {
+        const { searchText,  bookmarkedOnly, selectedGenre, movies}
         return (
             <div>
                 <SearchBar
@@ -24,6 +34,10 @@ class MovieLibrary extends React.Component {
                 <AddMovie
                     onClick={ this.onClick }
                 />
+                <MovieList
+                    movies={ movies }
+                />
+            
             </div>
             
         );
