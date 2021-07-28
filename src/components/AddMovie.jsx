4f100d37e7b5a18bd92f119743eb4d69c2aaba93
ElaineMoreira <seuemail@exemplo.br>
2021-07-28 10:05:26 -0300

    Adiciona requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,40 @@
-// implement AddMovie component here..
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const stateInitial = {
+        title: '',
+        subtitle: '',
+        imagePath: '',
+        storyline: '',
+        rating: 0,
+        genre: 'action',
+    };
+
+class AddMovie extends React.Component {
+    constructor() {
+        super();
+        this.state = stateInitial;
+        this.handleChange = this.handleChange.bind(this); // 1
+        this.handleClick = this.handleClick.bind(this); // 2
+}
+
+    handleChange({ target }) { // 1
+        this.setState({
+            [target.name]: target.value,
+        });
+    }
+
+    handleClick() { // 2
+        const { onClick } = this. props;
+        onClick(this.state);
+        this.setState(stateInitial); 
+    }
+
+    render() {
+        const { title, subtitle,  imagePath, storyline, rating, genre } = this.state;
+        return (
+        )
+    }
+};
+
+export default AddMovie;
