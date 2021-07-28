2b3ba2e338c8288c56cc8a0e6ad7c05277f6c1c6
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2021-07-28 18:02:26 -0300

    renderizando componente SearchBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import SearchBar from './components/SearchBar';
 import './App.css';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <MovieList movies={ movies } />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,14 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="contatiner-searchbar">
+        <input type="text" name="filtro" id="filtro" />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
