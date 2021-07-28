ba30113edd3d8f0086862b4073fd6302dc6ef396
ElaineMoreira <seuemail@exemplo.br>
2021-07-28 17:07:16 -0300

    Algumas alteraçoes no MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,45 +7,40 @@ import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
     constructor(props) {
-    
-    super(props);
-
-    const { movies } = this.props;
-
-    this.state = {
+      super(props);
+      const { movies } = this.props;
+  
+      this.state = {
         searchText: '',
         bookmarkedOnly: false,
-        selectedGenre:'',
+        selectedGenre: '',
         movies,
+      };
     }
-}
-    render() {
-        const { searchText,  bookmarkedOnly, selectedGenre, movies}
-        return (
-            <div>
-                <SearchBar
-                    searchText={ searchText }
-                    onSearchTextChange={ this.onSearchTextChange }
-                    bookmarkedOnly={ bookmarkedOnly }
-                    onBookmarkedChange={ this.onBookmarkedChange }
-                    selectedGenre={ selectedGenre }
-                    onSelectedGenreChange={ this.onSelectedGenreChange }
-                />
-                <AddMovie
-                    onClick={ this.onClick }
-                />
-                <MovieList
-                    movies={ movies }
-                />
-            
-            </div>
-            
-        );
-    }
+render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar
+          searchText={ searchText.props }
+          bookmarkedOnly={ bookmarkedOnly.props }
+          selectedGenre={ selectedGenre.props }
+          movies
+        />
+        <MovieList
+          movies={ movies }
+        />
+        <AddMovie onClick={ this.onClick } 
+        />
+      </div>
+    );
+  }
 }
 
 MovieLibrary.propTypes = {
-    movies: PropTypes.array,
-}.isRequired;
+  movies: PropTypes.arrayOf().isRequired,
+};
 
-export default MovieLibrary;
+export default MovieLibrary
