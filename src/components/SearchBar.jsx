dea0284a8e9a2adb32d9a41d4493d76d3d26815b
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-07-27 21:16:38 -0300

    Requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,10 +4,19 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div>
-        <h1>Search</h1>
+        <form data-testid="search-bar-form">
+          <input type="text" />
+        </form>
       </div>
     );
   }
 }
 
 export default SearchBar;
+// 2 - Renderize um formulário dentro de <SearchBar />
+// Dentro desse formulário haverá campos usados na filtragem de cartões.
+
+// Esse formulário deve apresentar o atributo data-testid="search-bar-form"
+// O que será verificado:
+
+// Será validado se é renderizado 1, e apenas 1, form dentro de <SearchBar />
