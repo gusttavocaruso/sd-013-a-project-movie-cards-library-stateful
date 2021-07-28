669dc8b0e0831e4716e3a62e7b2a36036b23e880
Leonardo Santos <lcds90@gmail.com>
2021-07-28 14:47:18 -0300

    addMovie with map and object literal input

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -25,66 +25,69 @@ class AddMovie extends React.Component {
     });
   }
 
-  // eslint-disable-next-line max-lines-per-function
+  generateInput(type, informations, fn) {
+    const { testId, name, value, label, options = [] } = informations;
+    const inputToChoose = {
+      text:
+  <InputText testId={ testId } name={ name } value={ value } onChange={ fn }>
+    { label }
+  </InputText>,
+      number:
+  <InputNumber testId={ testId } name={ name } value={ value } onChange={ fn }>
+    { label }
+  </InputNumber>,
+      textarea:
+  <InputTextarea testId={ testId } name={ name } value={ value } onChange={ fn }>
+    { label }
+  </InputTextarea>,
+      select:
+  <InputSelect
+    testId={ testId }
+    name={ name }
+    value={ value }
+    onChange={ fn }
+    options={ options }
+  >
+    {label}
+  </InputSelect>,
+    };
+    return inputToChoose[type];
+  }
+
   render() {
-    const { handleChange, state } = this;
+    const { handleChange, generateInput, state } = this;
     const { subtitle, title, imagePath, storyline, rating, genre } = state;
+    const texts = [
+      { testId: 'title-input', name: 'title', value: title, label: 'Título' },
+      { testId: 'subtitle-input', name: 'subtitle', value: subtitle, label: 'Subtítulo' },
+      { testId: 'image-input', name: 'imagePath', value: imagePath, label: 'Imagem' },
+    ];
+    const numbers = [{ testId: 'rating-input',
+      name: 'rating',
+      value: rating,
+      label: 'Avaliação' }];
+    const textareas = [{ testId: 'storyline-input',
+      name: 'storyline',
+      value: storyline,
+      label: 'Sinopse' },
+    ];
+    const genres = [
+      { text: 'Ação', value: 'action' },
+      { text: 'Comédia', value: 'comedy' },
+      { text: 'Suspense', value: 'thriller' },
+    ];
+    const selects = [{ testId: 'genre-input',
+      name: 'genre',
+      value: genre,
+      label: 'Gênero',
+      options: { info: genres,
+        name: 'genre' } }];
     return (
       <form data-testid="add-movie-form">
-        <InputText
-          testId="title-input"
-          name="title"
-          value={ title }
-          onChange={ handleChange }
-        >
-          Título
-        </InputText>
-        <InputText
-          testId="subtitle-input"
-          name="subtitle"
-          value={ subtitle }
-          onChange={ handleChange }
-        >
-          Subtítulo
-        </InputText>
-        <InputText
-          testId="image-input"
-          name="imagePath"
-          value={ imagePath }
-          onChange={ handleChange }
-        >
-          Imagem
-        </InputText>
-        <InputNumber
-          testId="rating-input"
-          name="rating"
-          value={ rating }
-          onChange={ handleChange }
-        >
-          Avaliação
-        </InputNumber>
-        <InputTextarea
-          testId="storyline-input"
-          name="storyline"
-          value={ storyline }
-          onChange={ handleChange }
-        >
-          Sinopse
-        </InputTextarea>
-        <InputSelect
-          testId="genre-input"
-          name="genre"
-          value={ genre }
-          onChange={ handleChange }
-          options={ { info: [
-            { text: 'Ação', value: 'action' },
-            { text: 'Comédia', value: 'comedy' },
-            { text: 'Suspense', value: 'thriller' },
-          ],
-          name: 'genre' } }
-        >
-          Gênero
-        </InputSelect>
+        {texts.map((i) => generateInput('text', i, handleChange))}
+        {numbers.map((i) => generateInput('number', i, handleChange))}
+        {textareas.map((i) => generateInput('textarea', i, handleChange))}
+        {selects.map((i) => generateInput('select', i, handleChange))}
       </form>
     );
   }
