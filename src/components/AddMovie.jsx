6d89294256ed69284d940cdba57e5906ef5d5dbb
junglejf <jfcchagas@id.uff.br>
2021-07-29 03:31:07 -0300

    Requisito 10,11,12,13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -45,6 +45,51 @@ class AddMovie extends Component {
             onChange={ this.changeStateProperty.bind(this) }
           />
         </label>
+        <label data-testid="image-input-label" htmlFor="image-input">
+          Imagem:
+          <input
+            data-testid="image-input"
+            property="imagePath"
+            type="text"
+            value={ this.state.imagePath }
+            onChange={ this.changeStateProperty.bind(this) }
+          />
+        </label>
+        <label data-testid="storyline-input-label" htmlFor="storyline-input">
+          Sinopse:
+          <textarea
+            data-testid="storyline-input"
+            property="storyline"
+            type="text"
+            value={ this.state.storyline }
+            onChange={ this.changeStateProperty.bind(this) }
+            rows="4"
+            cols="50"
+          />
+        </label>
+        <label data-testid="rating-input-label" htmlFor="rating-input">
+          Avaliação:
+          <input
+            data-testid="rating-input"
+            property="rating"
+            type="number"
+            value={ this.state.rating }
+            onChange={ this.changeStateProperty.bind(this) }
+          />
+        </label>
+        <label data-testid="genre-input-label" htmlFor="genre-input">
+          Gênero:
+          <select
+            data-testid="genre-input"
+            property="genre"
+            value={ this.state.genre }
+            onChange={ this.changeStateProperty.bind(this) }
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
