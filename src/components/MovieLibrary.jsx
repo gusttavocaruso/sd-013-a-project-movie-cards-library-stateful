10b2c33f2d32d7cb7cd17d283d03863f764331ac
Lanai Conceição <conceicao.lanai@gmail.com>
2021-07-29 07:57:47 -0300

    Requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,14 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        Teste
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
