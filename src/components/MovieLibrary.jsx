7298c9471dc66870cf5b68ceb32931a746e09206
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2021-07-29 16:45:04 -0300

    Resolve o requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
     const { movies } = this.props;
 
-    return(
-      <div />
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name } = event.target;
+    const value = event.target.type === 'checkbox'
+      ? event.target.checked : event.target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  searchByText = () => {
+    const { searchText, movies } = this.state;
+    const searchLower = searchText.toLowerCase();
+    const filteredByText = movies.filter((movie) => (searchText
+      ? movie.title.toLowerCase().includes(searchLower)
+      || movie.subtitle.toLowerCase().includes(searchLower)
+      || movie.storyline.toLowerCase().includes(searchLower) : movies));
+
+    return filteredByText;
+  }
+
+  searchByBookmarked = () => {
+    const { bookmarkedOnly } = this.state;
+    const filteredList = this.searchByText();
+    return filteredList.filter((item) => (
+      bookmarkedOnly ? item.bookmarked === true : filteredList));
+  }
+
+  searchByGenre = () => {
+    const { selectedGenre } = this.state;
+    const filteredList = this.searchByBookmarked();
+    return filteredList.filter((item) => (
+      selectedGenre ? item.genre === selectedGenre : filteredList));
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    return (
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleChange }
+        />
+        <MovieList movies={ this.searchByGenre() } />
+      </div>
     );
   }
 }
