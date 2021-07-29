8c402aede6b153847b4edbcf37ebf10c3334c10e
Diego Vilela <vilelaphp@gmail.com>
2021-07-29 11:59:32 -0300

    Implement requirements 15 to 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,48 @@
-// implement MovieLibrary component here
+import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: "",
+      bookmarkedOnly: false,
+      selectedGenre: "",
+      movies: movies,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange({ target }) {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+
+    return(
+      <section>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleInputChange }
+          onBookmarkedChange={ this.handleInputChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onSelectedGenreChange={ this.handleInputChange }
+          selectedGenre={ selectedGenre }
+        />
+        <MovieList movies={ movies } />
+      </section>
+    );
+  }
+}
+
+export default MovieLibrary;
