fdf7564c9128d0c40e9c0d50992e7444d08858df
Breno Lopes <brenokos4@gmail.com>
2021-07-28 21:11:32 -0300

    add 15... parte da 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,39 @@
-// implement MovieLibrary component here
+/* eslint-disable react/prop-types */
+import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import movies from '../data';
+
+class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      /* movies: props.movies, */
+    };
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre} = this.state;
+    return (
+      <>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList
+          movies={ movies }
+        />
+      </>
+    );
+  }
+}
+
+export default MovieLibrary;
