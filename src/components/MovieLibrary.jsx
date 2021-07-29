f87e0c5e5df82b456381bd068b362fe79062a803
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-07-29 17:55:51 -0300

    working on req 14, 17, 18 e 19

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -4,12 +4,39 @@ import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+// import movies from '../data';
 
 class MovieLibrary extends Component {
   constructor() {
     super();
 
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
     this.onClick = this.onClick.bind(this);
+
+    // Considerando que esse componente é pai da SearchBar, o Estado Inicial é declarado nele:
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+
+  onSearchTextChange({ target }) {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ searchText: value });
+    // movies.filter((movie) => movie.title.includes(value)).map((movie) => <MovieList movies={movie} key={movie.title} />)
+  }
+
+  onBookmarkedChange({ target }) {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ bookmarkedOnly: value });
+  }
+
+  onSelectedGenreChange({ target }) {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ selectedGenre: value });
   }
 
   onClick() {
@@ -18,39 +45,47 @@ class MovieLibrary extends Component {
 
   render() {
     // const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar
-          searchText=""
-          onSearchTextChange={ (() => {}) }
-          bookmarkedOnly={ false }
-          onBookmarkedChange={ () => {} }
-          selectedGenre=""
-          onSelectedGenreChange={ () => {} }
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
         />
         <AddMovie
-          value={ this.State }
+          // value={ this.State }
           onClick={ this.onClick }
           handleOnChange={ this.handleOnChange }
         />
-        <MovieList movies={ movies } />
+        <MovieList
+          movies={ movies }
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
       </div>
     );
   }
 }
 
 MovieLibrary.propTypes = {
-  movies: PropTypes.shape({
-    title: PropTypes.string,
-    subtitle: PropTypes.string,
-    storyline: PropTypes.string,
-    rating: PropTypes.number,
-    imagePath: PropTypes.string,
-    bookmarked: PropTypes.bool,
-    genre: PropTypes.string,
-  }).isRequired,
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+      bookmarked: PropTypes.bool,
+      genre: PropTypes.string,
+    }),
+  ).isRequired,
 };
 
 export default MovieLibrary;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,10 +6,10 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-
+    // searchText, bookmarkedOnly, selectedGenre
     return (
       <div data-testid="movie-list" className="movie-list">
-        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }
@@ -19,6 +19,9 @@ MovieList.propTypes = {
   movies: PropTypes.arrayOf(
     PropTypes.object,
   ).isRequired,
+  // searchText: PropTypes.string.isRequired,
+  // bookmarkedOnly: PropTypes.bool.isRequired,
+  // selectedGenre: PropTypes.string.isRequired,
 };
 
 export default MovieList;
