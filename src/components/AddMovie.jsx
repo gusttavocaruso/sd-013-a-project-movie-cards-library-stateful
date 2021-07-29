f90dab576282788631499939100d7a8b5807f26e
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-07-29 11:26:38 -0300

    add 8th requirement

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,24 +3,48 @@ import React from 'react';
 class AddMovie extends React.Component {
   constructor() {
     super();
+
     this.state = {
-      subtitle: '',
       title: '',
-      imagePath: '',
-      storyLine: '',
-      rating: 0,
-      genre: 'action',
+      // subtitle: '',
+      // imagePath: '',
+      // storyLine: '',
+      // rating: 0,
+      // genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
     const {
-      title, subtitle, imagePath, storyLine, rating, genre,
+      title, /* subtitle, imagePath, storyLine, rating, genre, */
     } = this.state;
 
     return (
       <form action="" data-testid="add-movie-form">
-        somefkcinthing
+
+        <label htmlFor="addMovie-title" data-testid="title-input-label">
+          Título:
+          <input
+            id="addMovie-title"
+            data-testid="title-input"
+            type="text"
+            name={ title }
+            value={ this.title }
+            onChange={ this.handleChange }
+          />
+        </label>
+
       </form>
     );
   }
