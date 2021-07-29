3ff2cc8fb2916a0e25077ae24a895f417e781b6d
Juliane Marques <mqs.juliane@gmail.com>
2021-07-29 10:01:51 -0300

    Arruma Lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+
 class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
