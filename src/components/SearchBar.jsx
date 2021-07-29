0a01179fce55aa0a35ea7af261d83b9c130961eb
Adriana Barreto <adri92barreto123@gmail.com>
2021-07-29 13:33:25 -0300

    Executa terceira e quarta questao

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
     return (
       <section>
         <form data-testid="search-bar-form">
-          <label htmlFor="name">
-            <input type="text" name="name" />
+          <label htmlFor="Inclui o texto:" data-testid="text-input-label">
+            Inclui o texto
+            <input
+              type="text"
+              name="Inclui o texto:"
+              data-testid="text-input"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+            />
+          </label>
+          <label htmlFor="check" data-testid="checkbox-input-label">
+            <input
+              type="checkbox"
+              name="check"
+              id=""
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+              data-testid="checkbox-input"
+            />
+            Mostrar somente favoritos
+          </label>
+          <label htmlFor="genero" data-testid="select-input-label">
+            <select
+              name="genero"
+              id=""
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+            >
+              <option value="" data-testid="select-option">Todos</option>
+              <option value="action" data-testid="select-option">Ação</option>
+              <option value="comedy" data-testid="select-option">Comédia</option>
+              <option value="thriller" data-testid="select-option">Suspense</option>
+            </select>
           </label>
         </form>
       </section>
@@ -14,15 +53,13 @@ class SearchBar extends React.Component {
   }
 }
 
-// SearchBar.propTypes = {
-//   movie: PropTypes.shape({
-//     searchText: PropTypes.string,
-//     onSearchTextChange: PropTypes.callback,
-//     bookmarkedOnly: PropTypes.boolean,
-//     onBookmarkedChange: PropTypes.callback,
-//     selectedGenre: PropTypes.string,
-//     onSelectedGenreChange: PropTypes.callback,
-//   }).isRequired,
-// };
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.callback,
+  bookmarkedOnly: PropTypes.boolean,
+  onBookmarkedChange: PropTypes.callback,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.callback,
+}.isRequired;
 
 export default SearchBar;
