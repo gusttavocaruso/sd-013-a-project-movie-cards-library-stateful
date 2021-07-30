541407d059eed42f898f8e45f6e35877ba824c3c
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-07-30 16:06:07 -0300

    Erro 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-
 import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
     const { rating, handleChange } = this.props;
     return (
-      <div>
-        <label htmlFor="teste" data-testid="rating-input-label">
-          Avaliação
-          <input
-            type="number"
-            name="rating"
-            id="teste"
-            value={ rating }
-            onChange={ handleChange }
-            data-testid="rating-input"
-          />
-        </label>
-      </div>
+      <label htmlFor="teste" data-testid="rating-input-label">
+        Avaliação
+        <input
+          type="number"
+          name="rating"
+          id="teste"
+          value={ rating }
+          onChange={ handleChange }
+          data-testid="rating-input"
+        />
+      </label>
     );
   }
 }
