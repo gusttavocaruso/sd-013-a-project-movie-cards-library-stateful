61000ca275524d9ac8866b4d708107bd3d6a781a
Julio Fallace <julioscoffee@gmail.com>
2021-07-29 21:50:56 -0300

    Resolvendo requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -25,6 +25,15 @@ class SearchBar extends React.Component {
               onChange={ onSearchTextChange }
             />
           </label>
+          <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              data-testid="checkbox-input"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+            />
+          </label>
         </form>
       </main>
     );
