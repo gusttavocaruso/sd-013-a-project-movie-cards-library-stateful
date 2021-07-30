5a8952dcf36eb577d31af685682c49c478a30c73
Julio Fallace <julioscoffee@gmail.com>
2021-07-30 19:35:50 -0300

    resolvido requisitos 6 e 7. Requisito 8 em andamento comentado para pular lint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,38 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     subtitle: '',
+  //     title: '',
+  //     imagePath: '',
+  //     storyline: '',
+  //     rating: 0,
+  //     genre: 'action',
+  //   };
+  // }
+
+  render() {
+    // const { onClick } = this.props;
+
+    return (
+      <main>
+        <form method="post" data-testid="add-movie-form">
+          <label htmlFor="title-input" data-testid="title-input-label">
+            Título
+            <input type="text" id="title-input" data-testid="title-input" onChange='' value='' />
+          </label>
+        </form>
+      </main>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func,
+}.isRequired;
+
+export default AddMovie;
