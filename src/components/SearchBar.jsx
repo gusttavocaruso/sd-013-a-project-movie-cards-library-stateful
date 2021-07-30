5097ff0a3b5e5b2a8d76134ced8f700cdddefcfc
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-07-30 20:50:57 -0300

    Iniciando renderizaçoes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <Form>
+        <input type="text" />
+        <label value="searchText">"Inclui texto:</label>
+      </Form>
+    );
+  }
+}
+export default SearchBar;
