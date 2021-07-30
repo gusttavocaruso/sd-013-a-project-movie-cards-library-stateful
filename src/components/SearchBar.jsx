e392902a45c229e23d470619d7a7489e5d122b87
Julio Fallace <julioscoffee@gmail.com>
2021-07-29 22:19:28 -0300

    resolvendo requisito 5. Suite de testes SearchBar completo!

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -20,6 +20,7 @@ class SearchBar extends React.Component {
             Inclui o texto:
             <input
               type="text"
+              id="text-input"
               data-testid="text-input"
               value={ searchText }
               onChange={ onSearchTextChange }
@@ -29,10 +30,24 @@ class SearchBar extends React.Component {
             Mostrar somente favoritos
             <input
               type="checkbox"
+              id="checkbox-input"
               data-testid="checkbox-input"
               checked={ bookmarkedOnly }
               onChange={ onBookmarkedChange }
             />
+          </label>
+          <label htmlFor="select-input" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              data-testid="select-input"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+            >
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
           </label>
         </form>
       </main>
