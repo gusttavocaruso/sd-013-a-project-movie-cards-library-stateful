eb9b93f992158a36201588c35ee77944f8c06e6e
suco <francalucas937@gmail.com>
2021-07-31 12:14:37 -0300

    fiz alguma coisa

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,52 @@
 // implement MovieLibrary component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+// import MovieList from './MovieList';
+// import AddMovie from './AddMovie';
+// import Movies from '../data';
+
+class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      // searchText: '',
+      // bookmarkedOnly: false,
+      // selectedGenre: '',
+      // movies: '',
+    };
+  }
+
+  callback1() {
+    return 'asd';
+  }
+
+  callback2() {
+    return 'asd';
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          searchText=""
+          onSearchTextChange={ this.callback1 }
+          bookmarkedOnly={ false }
+          selectedGenre=""
+          onSelectedGenreChange={ this.callback2 }
+        />
+
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  // searchText: PropTypes.string.isRequired,
+  // bookmarkedOnly: PropTypes.bool.isRequired,
+  // selectedGenre: PropTypes.string.isRequired,
+  // movies: PropTypes.array.isRequired,
+};
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,44 @@
-// implement SearchBar component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    // const { searchText } = this.props;
+    // const { onSearchTextChange } = this.props;
+    console.log(this.props);
+
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="input">
+          Inclui o texto:
+          <input
+            type="text"
+            // value={ searchText }
+            // onChange={ onSearchTextChange }
+            data-testid="text-input"
+            id="input"
+          />
+        </label>
+
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  // searchText: PropTypes.string.isRequired,
+  // onSearchTextChange: PropTypes.func.isRequired,
+  // bookmarkedOnly: PropTypes.bool.isRequired,
+  // selectedGenre: PropTypes.string.isRequired,
+  // onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
