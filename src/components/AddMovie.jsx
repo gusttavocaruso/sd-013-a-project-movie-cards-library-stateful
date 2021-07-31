d0e6a61a5f9535865713a306136d2263d17b653c
PC <sthecso>
2021-07-30 21:00:00 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,12 +12,27 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({ [target.name]: target.value });
   }
 
   render() {
     return (
       <div>
-        <form data-testid="add-movie-form" />
+        <form data-testid="add-movie-form">
+          <label htmlFor="add-movie" data-testid="title-input-label">
+            Título
+            <input
+              data-testid="title-input"
+              id="add-movie"
+              type="text"
+              onChange={this.handleChange} />
+          </label>
+        </form>
       </div>
     );
   }
