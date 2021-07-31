d4ecf0b38853c2659c5f6a0b5eb968ab530b2674
LuizFJP <luizfjportela@gmail.com>
2021-07-31 11:42:45 -0300

    requirement 15 and 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,17 @@
-//
+import React, { Component } from 'react';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
