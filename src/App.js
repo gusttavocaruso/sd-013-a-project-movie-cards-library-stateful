f849a2b4d3c7bd4e407a83ef7195da20f9bbf8a9
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-07-31 14:12:24 -0300

    renderiza componente no app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './styles/App.css';
 
-import movies from './data';
-
 import Header from './components/Header';
-import MovieList from './components/MovieList';
+
+import MovieLibrary from './components/MovieLibrary';
+
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movies={ movies } />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
