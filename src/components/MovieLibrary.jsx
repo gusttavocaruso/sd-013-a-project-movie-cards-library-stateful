911784536b781f2a97c84898489294ed663d4506
Ygor <ygormaiaa1@gmail.com>
2021-08-01 19:54:03 -0300

    Requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
+import movies from '../data';
 
 class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: movies,
+    };
+  }
+
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <SearchBar />
