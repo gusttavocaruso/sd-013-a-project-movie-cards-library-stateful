893587276a4b2f3246da6f3e50fdc67d0ca3b3ce
turtore <brunosap@gmail.com>
2021-08-01 16:55:54 -0300

    resolvido button com uma callback

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -69,22 +69,18 @@ class AddMovie extends Component {
               Título
               {this.aInput('text', 'title', 'title-input', title)}
             </label>
-
             <label htmlFor="subtitulo" data-testid="subtitle-input-label">
               Subtítulo
               {this.aInput('text', 'subtitle', 'subtitle-input', subtitle)}
             </label>
-
             <label htmlFor="imagePath" data-testid="image-input-label">
               Imagem
               {this.aInput('text', 'imagePath', 'image-input', imagePath)}
             </label>
-
             <label htmlFor="storyline" data-testid="storyline-input-label">
               Sinopse
               {this.aTextArea('storyline-input', storyline, 'storyline')}
             </label>
-
             <label htmlFor="rating" data-testid="rating-input-label">
               Avaliação
               {this.aInput('number', 'rating', 'rating-input', rating)}
@@ -104,7 +100,11 @@ class AddMovie extends Component {
               </select>
             </label>
 
-            <button type="button" onClick={ this.reset } data-testid="send-button">
+            <button
+              type="button"
+              onClick={ () => this.reset() }
+              data-testid="send-button"
+            >
               Adicionar filme
             </button>
           </form>
