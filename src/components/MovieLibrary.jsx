31dead3fdda0109c43413509e0562b921e20853e
Walace Nascimento <wlacens@hotmail.com>
2021-08-01 00:29:42 -0300

    Resolve os requisitos 1, 2, e 3.

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,16 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+import MoveList from './MovieList';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <MoveList />
+        <SearchBar />
+      </div>
+    );
+  }
+}
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,38 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
+    return (
+
+      <form data-testid="search-bar-form">
+        <label htmlFor=" seach-text" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            id="seach-text"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+//   searchText: PropTypes.string,
+//   onSearchTextChange: Proptypes.function,
+//   bookmarkedOnly: PropTypes.boll,
+//   onBookmarkedChange: PropTypes.function,
+//   selectedGenre: PropTypes.string,
+//   onSelectedGenreChange: PropTypes.function,
+}.isRequired;
+
+export default SearchBar;
