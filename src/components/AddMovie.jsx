9e6efbe90b75f097574deb9dbb2daa76fa4d29d6
Walace Nascimento <wlacens@hotmail.com>
2021-08-01 02:02:17 -0300

    Resolve o requisito 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,28 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+const InitialState = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = InitialState;
+  }
+
+  render() {
+    return (
+      <div>
+        <form data-testid="add-movie-form"> </form>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
