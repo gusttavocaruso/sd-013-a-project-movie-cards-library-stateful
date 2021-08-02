e2b9b52c669b9acc44821e488bbf54cad091c321
Julio Fallace <julioscoffee@gmail.com>
2021-08-02 15:54:20 -0300

    corrigindo lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,7 +1,6 @@
 // implement MovieLibrary component here
 import React from 'react';
 import AddMovie from './AddMovie';
-import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
