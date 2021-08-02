871792b3112cf7dd991559d2f2c19a226dc342c4
Erick Santos <erickosantos.dev@gmail.com>
2021-08-02 17:06:48 -0300

    16 - Configure o estado inicial do componente <MovieLibrary />

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies
+    }
+  }
+  
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
-      <div />
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
+        <MovieList
+          movies={ movies }
+        />
+      </div>
     );
   }
 }
