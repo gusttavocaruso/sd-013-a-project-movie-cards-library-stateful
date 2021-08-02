5a53f2c8205e9a00828a56d8c210e218c47b4bf5
Heleny Flavia <flaviaanahmias@gmail.com>
2021-08-01 21:15:20 -0400

    Requisito 12

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -28,7 +28,7 @@ class AddMovie extends React.Component {
 
   render() {
     const { onClick } = this.props;
-    const { title, subtitle, imagePath, storyline } = this.state;
+    const { title, subtitle, imagePath, storyline, rating } = this.state;
 
     return (
         <form data-testid="add-movie-form" action=""> 
@@ -51,6 +51,11 @@ class AddMovie extends React.Component {
             // Requisito 11
             <label data-testid="storyline-input-label" htmlFor="storyline-input"> Sinopse
                 <textarea data-testid="storyline-input" name="storyline" value={ storyline } id="storyline-input" onChange={ this.handleChange } cols="30" rows="10"></textarea>
+            </label>
+
+            // Requisito 12
+            <label data-testid="rating-input-label" htmlFor="rating-input"> Avaliação
+                <input data-testid="rating-input" name="rating" type="number" value={ rating } id="rating-input" onChange={ this.handleChange } />
             </label>
         </form>
     );
