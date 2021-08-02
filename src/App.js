906f4359bd1d683224dc2473cceb9cac0a78c7df
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-08-02 17:01:14 -0300

    Resolve o requisito 6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
 import MovieLibrary from './components/MovieLibrary';
+import SearchBar from './components/SearchBar';
 import data from './data';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <MovieLibrary movies={ data } />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MovieLibrary movies={ data } />
+      <SearchBar />
+    </div>
+  );
 }
 
 export default App;
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,42 @@
-// implement AddMovie component here
+import React from 'react';
+
+const propriedades = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = propriedades;
+    // this.state = {
+    //   subtitle: '',
+    //   title: '',
+    //   imagePath: '',
+    //   storyline: '',
+    //   rating: 0,
+    //   genre: 'action',
+    // };
+  }
+
+  render() {
+    // const {
+    //   subtitle,
+    //   title,
+    //   imagePath,
+    //   storyline,
+    //   rating,
+    //   genre,
+    // } = this.state;
+
+    return (
+      <form>teste</form>
+    );
+  }
+}
+
+export default AddMovie;
