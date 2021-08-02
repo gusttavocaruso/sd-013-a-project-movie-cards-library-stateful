1ee5256ae1731c7088018ccb2531db67bac690dc
Heleny Flavia <flaviaanahmias@gmail.com>
2021-08-01 20:44:08 -0400

    Requisito 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,34 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    } // Requisito 6
+  }
+
+  render() {
+    const { onClick } = this.props;
+
+    return (
+        <form data-testid="add-movie-form"> // Requisito 7
+
+        </form>
+    );
+  }
+}
+
+AddMovie.PropTypes = {
+  onClick: PropTypes.func,
+};
+
+export default AddMovie; 
