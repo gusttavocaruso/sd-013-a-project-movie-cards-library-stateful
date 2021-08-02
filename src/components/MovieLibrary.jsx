e087c47e349cb0f6f9c28a549c9ae4b66fe10bb8
Erick Santos <erickosantos.dev@gmail.com>
2021-08-02 17:53:19 -0300

    lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
@@ -12,25 +14,45 @@ class MovieLibrary extends React.Component {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies
-    }
+      movies,
+    };
+
+    this.onChange = this.onChange.bind(this);
   }
-  
+
+  // Ideia de refatoração
+  // https://github.com/tryber/sd-013-a-project-movie-cards-library-stateful/pull/20
+  onChange = ({ target }) => {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
         <SearchBar
           searchText={ searchText }
+          onSearchTextChange={ this.onChange }
           bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onChange }
           selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onChange }
         />
         <MovieList
           movies={ movies }
         />
+        <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.shape([]).isRequired,
+};
 
 export default MovieLibrary;
