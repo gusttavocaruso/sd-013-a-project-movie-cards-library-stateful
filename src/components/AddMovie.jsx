0044e44f59ff9c80ab3de8912b4b3bc5edfbf794
PC <sthecso>
2021-08-01 22:03:12 -0300

    Requisito 14

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Genre from './AddGenre';
 import Img from './AddImage';
 import Rating from './AddRating';
 import Sinopse from './AddSinopse';
 import Subtitle from './AddSubtitle';
 import Title from './AddTitle';
+import Button from './Button';
 
 class AddMovie extends React.Component {
   constructor(props) {
@@ -20,10 +22,24 @@ class AddMovie extends React.Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.submitButton = this.submitButton.bind(this);
   }
 
   handleChange({ target: { name, value } }) {
     this.setState({ [name]: value });
+  }
+
+  submitButton() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
   }
 
   render() {
@@ -37,9 +53,15 @@ class AddMovie extends React.Component {
           <Sinopse value={ storyline } onChange={ this.handleChange } />
           <Rating value={ rating } onChange={ this.handleChange } />
           <Genre value={ genre } onChange={ this.handleChange } />
+          <Button onClick={ this.submitButton } />
         </form>
       </div>
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
 export default AddMovie;
