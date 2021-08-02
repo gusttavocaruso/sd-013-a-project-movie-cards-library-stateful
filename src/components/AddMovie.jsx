fd134373f242139358c983187719d6f038207f4c
Sethano <vitorcard1720@gmail.com>
2021-08-02 17:46:31 -0300

    so falta o 18

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,70 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Title from './AddMovieFormComponents/Title';
+import Subtitle from './AddMovieFormComponents/Subtitle';
+import ImagePath from './AddMovieFormComponents/ImagePath';
+import Storyline from './AddMovieFormComponents/Storyline';
+import Rating from './AddMovieFormComponents/Rating';
+import Genre from './AddMovieFormComponents/Genre';
+
+export default class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleChange =(event) => {
+    const { id, value } = event.target;
+    this.setState({ [id]: value });
+  }
+
+  handleClick = () => {
+    const { onClick } = this.props;
+    onClick(this.state);
+
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    const { handleChange, handleClick } = this;
+
+    return (
+      <form data-testid="add-movie-form">
+        <div>
+          <Title value={ title } onChange={ handleChange } />
+          <Subtitle value={ subtitle } onChange={ handleChange } />
+          <ImagePath value={ imagePath } onChange={ handleChange } />
+          <Storyline value={ storyline } onChange={ handleChange } />
+          <Rating value={ rating } onChange={ handleChange } />
+          <Genre value={ genre } onChange={ handleChange } />
+          <button
+            type="button"
+            data-testid="send-button"
+            onClick={ handleClick }
+          >
+            Adicionar filme
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,68 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+
+export default class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  search = (event) => {
+    this.setState({
+      searchText: event.target.value,
+    });
+  }
+
+  bookmarker = (event) => {
+    this.setState({
+      bookmarkedOnly: event.target.checked,
+    });
+  }
+
+  genre = (event) => {
+    this.setState({
+      selectedGenre: event.target.value,
+    });
+  }
+
+  handleClick = (newMovie) => {
+    const { movies } = this.state;
+    this.setState({ movies: [...movies, newMovie] });
+  };
+
+  // filter = () => {
+  // };
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const { search, bookmarker, genre, handleClick } = this;
+
+    return (
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ search }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ bookmarker }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ genre }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ handleClick } />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
