96a742809e4a47ffe4a8ad67f862ddaf4bac44e4
Isabella Antunes <bella.antunes2@gmail.com>
2021-08-02 08:46:21 -0300

    Requisito 17 e 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Components {
   constructor(props) {
@@ -11,12 +13,55 @@ class MovieLibrary extends React.Components {
       selectedGenre: 'action',
       movies,
     };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+  }
+
+  onSearchTextChange({ target }) {
+    const { value } = target;
+    this.setState({
+      searchText: value,
+    });
+  }
+
+  onBookmarkedChange({ target }) {
+    if (target.checked === true) {
+      this.setState({
+        bookmarkedOnly: true,
+      });
+    }
+  }
+
+  onSelectedGenreChange({ target }) {
+    const { value } = target;
+    this.setState({
+      selectedGenre: value,
+    });
   }
 
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+
+    const filterMovie = movies.filter(({ title, subtitle, storyline }) => (
+      title.toLowerCase().includes(searchText.toLowerCase())
+      || storyline.toLowerCase().includes(searchText.toLowerCase())
+      || subtitle.toLowerCase().includes(searchText.toLowerCase())))
+      .filter(({ bookmarked }) => (
+        bookmarkedOnly ? bookmarked === bookmarkedOnly : true))
+      .filter((movie) => movie.genre.includes(selectedGenre));
+
     return (
       <div>
-        teste
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          onSearchTextChange={ this.onSearchTextChange }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList movies={ filterMovie } />
       </div>
     );
   }
