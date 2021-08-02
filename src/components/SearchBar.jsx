6a5533de609a22843e4138e6d883a3419b879130
Heleny Flavia <flaviaanahmias@gmail.com>
2021-08-01 20:29:56 -0400

    Requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,17 +7,27 @@ class SearchBar extends React.Component {
     const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
 
     return (
-        <form data-testid="search-bar-form" action=""> // Requisito 2
-            <label data-testid="text-input-label" htmlFor="text-input"> // Requisito 3
-                Inclui o texto:
-                <imput data-testid="text-input" type="text" value={ searchText } onChange={ onSearchTextChange } />
-            </label>
+      <form data-testid="search-bar-form" action=""> // Requisito 2
+        <label data-testid="text-input-label" htmlFor="text-input"> // Requisito 3
+          Inclui o texto:
+          <imput data-testid="text-input" type="text" value={ searchText } onChange={ onSearchTextChange } />
+        </label>
 
-            <label data-testid="checkbox-input-label" htmlFor="checkbox-input" > // Requisito 4
-                Mostrar somente favoritos
-                <imput data-testid="checkbox-input" type="checkbox" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } />
-            </label>
-        </form>
+        <label data-testid="checkbox-input-label" htmlFor="checkbox-input" > // Requisito 4
+          Mostrar somente favoritos
+          <imput data-testid="checkbox-input" type="checkbox" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } />
+        </label>
+
+        <label data-testid="select-input-label" > // Requisito 5
+          Filtrar por gênero
+          <select data-testid="select-input" value={ selectedGenre } OnChange= { onSelectedGenreChange } >
+                <option data-testid="select-option" value="">Todos</option>
+                <option data-testid="select-option" value="action">Ação</option>
+                <option data-testid="select-option" value="comedy">Comédia</option>
+                <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
+        </label>
+    </form>
     );
   }
 }
