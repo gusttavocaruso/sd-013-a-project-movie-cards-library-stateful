84038b67633774d55685b4f43a730ea3a15d1923
João Paulo <jp.viegas@hotmail.com>
2021-08-02 15:40:14 -0300

    iniciando projeto/finalizando requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
 // implement SearchBar component here
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <div>
+      <form action="" />
+    </div>
+  );
+}
+
+export default SearchBar;
