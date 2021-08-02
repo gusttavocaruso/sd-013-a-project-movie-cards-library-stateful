0076f4109611e2fa3db66d152af02a88b50438d7
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2021-08-02 15:21:40 -0300

    Requisitos 1 e 2

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-
-import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div>
-        <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={this.props.movies} />
-        <AddMovie />
+        <SearchBar
+          searchText="uma string"
+          onSearchTextChange="uma callback"
+          bookmarkedOnly="um boolean"
+          onBookmarkedChange="uma callback"
+          selectedGenre="uma string"
+          onSelectedGenreChange="uma callback"
+        />
       </div>
     );
   }
