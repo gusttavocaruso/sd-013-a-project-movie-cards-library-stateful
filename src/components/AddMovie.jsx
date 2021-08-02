1b181dd1d63ca228e7908968da18f6aed8d74457
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-08-02 18:54:55 -0300

    Adiciona Requisitos de 4 a 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,53 @@
-// implement AddMovie component her
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { subtitle, title } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label
+          data-testid="title-input-label"
+          htmlFor="title-input"
+        >
+          Título
+          <input
+            type="text"
+            id="title-input"
+            value={ title }
+            data-testid="title-input"
+            onChange={ ({ target }) => this.state({ title: target.value }) }
+          />
+        </label>
+        <label
+          data-testid="subtitle-input-label"
+          htmlFor="subtitle-input"
+        >
+          Subtítulo
+          <input id="subtitle-input" data-testid="subtitle-input" value={ subtitle } onChange={ ({ target }) => this.setState({ subtitle: target.value }) }
+        />
+        </label>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,6 +6,10 @@ class SearchBar extends React.Component {
     const {
       searchText,
       onSearchTextChang,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
     } = this.props;
     return (
       <form data-testid="search-bar-form">
@@ -22,6 +26,36 @@ class SearchBar extends React.Component {
             onChange={ onSearchTextChang }
           />
         </label>
+        <label
+          data-testid="checkbox-input-label"
+          htmlFor="checkbox-label"
+        >
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            data-testid="checkbox-input"
+            id="checkbox-label"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+        <label
+          htmlFor="genre-options"
+          data-testid="select-input-label"
+        >
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            id="genre-options"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="Action">Ação</option>
+            <option data-testid="select-option" value="Comedy">Comédia</option>
+            <option data-testid="select-option" value="Suspense">Thriller</option>
+          </select>
+        </label>
       </form>
     );
   }
@@ -30,10 +64,16 @@ class SearchBar extends React.Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string,
   onSearchTextChang: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 SearchBar.defaultProps = {
   searchText: '',
+  bookmarkedOnly: false,
+  selectedGenre: '',
 };
 
 export default SearchBar;
