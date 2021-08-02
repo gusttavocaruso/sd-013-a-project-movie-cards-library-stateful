5903cdab623a7c817f4f90c6d16b1de276f4c38e
ArthurMGoulart <budiarthur@gmail.com>
2021-08-02 14:39:31 -0300

    Alterando local dos fields

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Field from './Field';
-import { fields } from '../data';
+import fields from './fields';
 
 class AddMovie extends React.Component {
   constructor() {
--- src/data.js
+++ src/data.js
@@ -46,44 +46,4 @@ const movies = [
   },
 ];
 
-const fields = [
-  {
-    labelTestId:"title-input-label",
-    htmlFor:"titulo",
-    text:"Título",
-    type:"text",
-    id:"titulo",
-    name:"title",
-    inputTestId:"title-input",
-  },
-  {
-    labelTestId:"subtitle-input-label",
-    htmlFor:"subTitulo",
-    text:"Subtítulo",
-    type:"text",
-    id:"subTitulo",
-    name:"subtitle",
-    inputTestId:"subtitle-input",
-  },
-  {
-    labelTestId:"image-input-label",
-    htmlFor:"imagem",
-    text:"Imagem",
-    type:"text",
-    id:"imagem",
-    name:"imagePath",
-    inputTestId:"image-input",
-  },
-  {
-    labelTestId:"rating-input-label",
-    htmlFor:"avaliacao",
-    text:"Avaliação",
-    type:"number",
-    id:"avaliacao",
-    name:"rating",
-    inputTestId:"rating-input",
-  },
-
-]
-
-export { movies, fields };
+export default movies;
