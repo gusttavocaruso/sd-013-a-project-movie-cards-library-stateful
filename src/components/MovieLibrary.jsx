b2fc49186b18ad4077bd273c608bfd77df6c1a72
PC <sthecso>
2021-08-01 22:42:31 -0300

    Requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-
-// import MovieList from './MovieList';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+import movies from '../data';
 
 class MovieLibrary extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: { movies },
+    };
+  }
 
   render() {
     return (
@@ -21,7 +28,7 @@ class MovieLibrary extends Component {
           selectedGenre=""
           onSelectedGenreChange={ () => { } }
         />
-        {/* <MovieList movies={ this.props.movies } /> */}
+        <MovieList movies={ this.props.movies } />
         <AddMovie onClick={ () => { } } />
       </div>
     );
