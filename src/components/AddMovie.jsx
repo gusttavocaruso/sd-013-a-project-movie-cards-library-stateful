95a6e3b8a4914040909535164d4146a46d66d23d
Emanoel Mattos <emanoelmattos13@gmail.com>
2021-08-03 00:07:17 -0300

    lint porque me odeias

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Title from './Title';
-import SubTitle from './Subtitle';
-import ImagePath from './ImagePath';
-import StoryLine from './StoryLine';
-import RatingInput from './RatingImput';
-import Genre from './Genre';
+import TitleInput from './Title';
+import SubTitleInput from './Subtitle';
+import ImagePathInput from './ImagePath';
+import StoryLineInput from './StoryLine';
+import RatingInputNumber from './RatingImput';
+import GenreInput from './Genre';
 
 class AddMovie extends React.Component {
   constructor(props) {
@@ -52,12 +52,12 @@ class AddMovie extends React.Component {
         className="addFormContainer"
         onSubmit={ this.handleClick }
       >
-        <Title value={ title } onChange={ this.handleDigit } />
-        <SubTitle value={ subtitle } onChange={ this.handleDigit } />
-        <ImagePath value={ imagePath } onChange={ this.handleDigit } />
-        <StoryLine value={ storyline } onChange={ this.handleDigit } />
-        <RatingInput value={ rating } onChange={ this.handleDigit } />
-        <Genre value={ genre } onChange={ this.handleDigit } />
+        <TitleInput value={ title } onChange={ this.handleDigit } />
+        <SubTitleInput value={ subtitle } onChange={ this.handleDigit } />
+        <ImagePathInput value={ imagePath } onChange={ this.handleDigit } />
+        <StoryLineInput value={ storyline } onChange={ this.handleDigit } />
+        <RatingInputNumber value={ rating } onChange={ this.handleDigit } />
+        <GenreInput value={ genre } onChange={ this.handleDigit } />
         <button
           data-testid="send-button"
           type="submit"
