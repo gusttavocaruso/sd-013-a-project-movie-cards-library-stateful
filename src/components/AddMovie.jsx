2353c5d32943765ed0a3125af6930e7710888a3c
Jackson Santana <onjacksm@gmail.com>
2021-08-03 19:29:25 -0300

    Adiciona requisitos 6, 7 e 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-class AddMovie extends React.Component {
-  
+class AddMovie extends Component {
   constructor() {
     super();
 
+    this.hadleChange = this.hadleChange.bind(this);
+    this.hadleOnClick = this.hadleOnClick.bind(this);
     this.state = {
-      subtitle: '',
       title: '',
+      subtitle: '',
       imagePath: '',
       storyline: '',
       rating: 0,
-      genre: 'action'
+      genre: 'action',
     };
-  };
+  }
+
+  hadleChange({ target }) { // event.target desconstruido
+    this.setState({
+      [target.id]: target.value,
+    });
+  }
+
+  hadleOnClick() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState(stateInicial);
+  }
 
   render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title">
+          Título
+          <input
+            type="text"
+            id="title"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.hadleChange }
+          />
+        </label>
       </form>
     );
   }
