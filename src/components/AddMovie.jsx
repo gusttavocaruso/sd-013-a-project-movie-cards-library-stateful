fca213f195d3f96edc23f09d8ad9f6c6396c814a
Jackson Santana <onjacksm@gmail.com>
2021-08-03 00:47:49 -0300

    Adiciona requisitos 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,2 +1,27 @@
-// implement AddMovie component here
-// Push
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action'
+    };
+  };
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
