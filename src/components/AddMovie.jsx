aace72f8da1713b587edf9b71f06af2a1d285f63
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-08-03 18:48:38 -0300

    Requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,14 +4,16 @@ import React from 'react';
 class AddMovie extends React.Component {
   render() {
     return (
-      <div>Teste</div>
+      <div>
+        <form data-testid="add-movie-form" />
+      </div>
     );
   }
 }
 
 export default AddMovie;
 
-// Esse componente renderizará um formulário que permite adicionar na biblioteca um novo cartão de filme, dadas as seguintes informações do novo filme:
+// 6- Esse componente renderizará um formulário que permite adicionar na biblioteca um novo cartão de filme, dadas as seguintes informações do novo filme:
 
 //   - subtítulo
 //   - título
@@ -44,3 +46,33 @@ export default AddMovie;
 
 // **O que será verificado:**
 //   - Será validado se o componente renderiza
+
+// ------------------------------------------
+
+// 7 - Renderize um formulário dentro de `<AddMovie />`
+
+// Dentro desse formulário haverá campos usados para preencher informações do novo cartão a ser adicionado na biblioteca.
+
+// **O que será verificado:**
+//   - Será validado se o componente renderiza 1, e apenas 1, form
+//   - O form deve conter o atributo `data-testid="add-movie-form"`
+
+// ------------------------------------------
+
+// ### 8 - Renderize um input do tipo texto dentro do formulário em `<AddMovie />` para obter o título do novo filme
+
+// - O input deve ter uma label associada com o texto: **"Título"**;
+
+// - Essa label deve apresentar o atributo `data-testid="title-input-label"`
+
+// - O input deve ter seu valor inicial provido pelo estado inicial do componente, via `title`;
+
+// - Esse input deve apresentar o atributo `data-testid="title-input"`
+
+// - A propriedade `onChange` deve atualizar o estado de `<AddMovie />`, atribuindo a `title` o atual título contido no input.
+
+// **O que será verificado:**
+//   - Será validado se o componente renderiza um input de texto para quem usa escrever o titulo do filme
+//   - Será validado se o componente renderiza a label "Título" para o input de titulo
+//   - Será validado se o estado inicial do titulo é "", ou seja, uma string vazia
+//   - Será validado se o valor do input de título muda quando algo é digitado por quem usa.
