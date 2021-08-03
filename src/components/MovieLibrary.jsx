b9e04a4986da8b203b27a18476d8dc4a648bb52f
PC <sthecso>
2021-08-02 23:33:47 -0300

    Requisitos do 17-19

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,38 +1,91 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
-import movies from '../data';
 
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
 
+    const { movies } = this.props;
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: { movies },
+      movies,
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.getState = this.getState.bind(this);
+    this.getBookMarked = this.getBookMarked.bind(this);
+    this.getSelectedGenre = this.getSelectedGenre.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target: { name, value, type, checked } }) {
+    const valueType = type === 'checkbox' ? checked : value;
+    this.setState({ [name]: valueType });
+  }
+
+  handleClick(movieObject) {
+    const { movies } = this.state;
+    this.setState({
+      movies: [...movies, movieObject],
+    });
+  }
+
+  getState() {
+    const { searchText, movies } = this.state;
+    const searchLower = searchText.toLowerCase();
+    const newList = movies.filter((movie) => (
+      searchText ? movie.title.toLowerCase().includes(searchLower)
+        || movie.subtitle.toLowerCase().includes(searchLower)
+        || movie.storyline.toLowerCase().includes(searchLower)
+        : movies
+    ));
+    return newList;
+  }
+
+  getBookMarked() {
+    const { bookmarkedOnly } = this.state;
+    const filtered = this.getState();
+    return filtered.filter((movie) => (
+      bookmarkedOnly ? movie.bookmarked === true
+        : filtered
+    ));
+  }
+
+  getSelectedGenre() {
+    const { selectedGenre } = this.state;
+    const filtered = this.getBookMarked();
+    return filtered.filter((movie) => (
+      selectedGenre ? movie.genre === selectedGenre
+        : filtered
+    ));
   }
 
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
-        <h2> My awesome movie library </h2>
         <SearchBar
-          searchText=""
-          onSearchTextChange={ () => { } }
-          bookMarkedOnly
-          onBookmarkedChange={ () => { } }
-          selectedGenre=""
-          onSelectedGenreChange={ () => { } }
+          searchText={ searchText }
+          onSearchTextChange={ this.handleChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleChange }
         />
-        <MovieList movies={ this.props.movies } />
-        <AddMovie onClick={ () => { } } />
+        <MovieList movies={ this.getSelectedGenre() } />
+        <AddMovie onClick={ this.handleClick } />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,47 +9,47 @@ class SearchBar extends React.Component {
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
-      onSelectedGenreChange,
-    } = this.props;
+      onSelectedGenreChange } = this.props;
     return (
-      <div>
-        <form data-testid="search-bar-form">
-          <label htmlFor="search-bar" data-testid="text-input-label">
-            Inclui o texto:
-            <input
-              data-testid="text-input"
-              type="text"
-              id="search-bar"
-              value={ searchText }
-              onChange={ onSearchTextChange }
-            />
-          </label>
-          <label htmlFor="bookmark-checkbox" data-testid="checkbox-input-label">
-            Mostrar somente favoritos
-            <input
-              data-testid="checkbox-input"
-              type="checkbox"
-              id="bookmark-checkbox"
-              checked={ bookmarkedOnly }
-              onChange={ onBookmarkedChange }
-            />
-          </label>
-          <label htmlFor="select-genre" data-testid="select-input-label">
-            Filtrar por gênero
-            <select
-              data-testid="select-input"
-              id="select-genre"
-              value={ selectedGenre }
-              onChange={ onSelectedGenreChange }
-            >
-              <option data-testid="select-option" value="">Todos</option>
-              <option data-testid="select-option" value="action">Ação</option>
-              <option data-testid="select-option" value="comedy">Comédia</option>
-              <option data-testid="select-option" value="thriller">Suspense</option>
-            </select>
-          </label>
-        </form>
-      </div>
+      <form data-testid="search-bar-form">
+        <label htmlFor="search-bar" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            type="text"
+            id="search-bar"
+            name="searchText"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <label htmlFor="bookmark-checkbox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            data-testid="checkbox-input"
+            type="checkbox"
+            id="bookmark-checkbox"
+            name="bookmarkedOnly"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+        <label htmlFor="select-genre" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            id="select-genre"
+            name="selectedGenre"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
