82495f3704b897c591a49bb4a983567528cffeb5
Lucas Lima <lucassauro.lt@gmail.com>
2021-08-02 22:57:34 -0300

    Corrige erros, novamente.

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      // searchText: '',
-      // bookmarkedOnlky: false,
-      // selectedGenre: '',
-      // movies: { movies },
-    };
-  }
+  // constructor() {
+  //   super();
+  //   // this.state = {
+  //   //   movies: { movies },
+  //   // };
+  // }
 
   render() {
-    // const { movies } = this.props;
+    const { movies } = this.props;
 
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={ movies } />
+        <MovieList data={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
-// MovieLibrary.propTypes = {
-// movies: PropTypes.arrayOf(PropTypes.obj),
-// };
+
+MovieLibrary.defaultProps = {
+  movies: { movies },
+};
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default MovieLibrary;
