72ea8ca1e90ea4c00045df763bf80bfc03dc6cd7
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-08-02 23:02:02 -0300

    Requisito 5

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
@@ -8,29 +9,60 @@ class MovieLibrary extends React.Component {
     super(props)
     this.state = {
       searchText: '',
+      onSearchTextChange: ({ target }) => {
+        const { name, value } = target;
+        this.setState({
+          [name]: value,
+        })
+      },
       bookmarkedOnly: false,
+      onBookmarkedChange: ({ target }) => {
+        this.setState({
+          bookmarkedOnly: target.checked
+        })
+      },
+
       selectedGenre: '',
+      onSelectedGenreChange: ({ target }) => {
+        const { value } = target;
+        this.setState({
+          selectedGenre: value,
+        })
+      },
       movies: props.movies,
     }
   }
 
   render() {
-    const { movies } = this.props; 
-    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange, movies } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar 
         searchText={ searchText }
+        onSearchTextChange={ onSearchTextChange }
         bookmarkedOnly={ bookmarkedOnly }
+        onBookmarkedChange={onBookmarkedChange}
         selectedGenre={ selectedGenre }
-        movies={ movies }
+        onSelectedGenreChange={ onSelectedGenreChange }
         />
         <MovieList movies={ movies } />
         <AddMovie />  
       </div>
     );
   }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  })).isRequired,
 }
 
 export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="text-input" data-testid="text-input-label">
           Inclui o texto:
-          <input type="text" value={ searchText } onChange='' data-testid="text-input" />
+          <input type="text" name="searchText" value={ searchText } onChange={ onSearchTextChange } data-testid="text-input" />
         </label>
         <label htmlFor="checkbox-input">
           Mostrar somente favoritos
-          <input type="checkbox" checked={ bookmarkedOnly } onChange={ } data-testid="checkbox-input" />
+          <input type="checkbox" name="bookmarkedOnly" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } data-testid="checkbox-input" />
         </label>
         <label htmlFor="select-input" data-testid="select-input-label">
           Filtrar por gênero
-          <select name="" value={ selectedGenre } onChange={ } data-testid="select-input">
+          <select name="selectedGenre" value={ selectedGenre } onChange={ onSelectedGenreChange } data-testid="select-input">
             <option value="">Todos</option>
             <option value="action">Ação</option>
             <option value="comedy">Comédia</option>
@@ -26,4 +28,14 @@ class SearchBar extends React.Component {
     );
   }
 }
+
+SearchBar.propTypes = {
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  onSearchTextChange: PropTypes.func,
+  onSelectedGenreChange: PropTypes.func,
+  searchText: PropTypes.string,
+  selectedGenre: PropTypes.string,
+}.isRequired;
+
 export default SearchBar;
