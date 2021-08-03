f1ad749dbb7a6d6f33c1a2fd5b71c40d945bf760
Jackson Santana <onjacksm@gmail.com>
2021-08-02 22:25:08 -0300

    Adiciona requisitos 4 e 5 e correção do Lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly,
-      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
 
     return (
       <form data-testid="search-bar-form">
@@ -14,8 +14,38 @@ class SearchBar extends React.Component {
             value={ searchText }
             onChange={ onSearchTextChange }
             data-testid="text-input"
+            id="text"
           />
         </label>
+
+        <label data-testid="checkbox-input-label" htmlFor="checkbox">
+          Mostrar somente favoritos
+          <input
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+            id="checkbox"
+            type="checkbox"
+          />
+        </label>
+
+        <label data-testid="select-input-label" htmlFor="select">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+            id="select"
+          >
+
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+
+          </select>
+        </label>
+
       </form>
     );
   }
@@ -31,3 +61,6 @@ SearchBar.propTypes = {
 }.isRequired;
 
 export default SearchBar;
+
+// Referências:
+//  htmlFOR: https://pt-br.reactjs.org/docs/dom-elements.html // https://stackoverflow.com/questions/22752116/react-ignores-for-attribute-of-the-label-element
