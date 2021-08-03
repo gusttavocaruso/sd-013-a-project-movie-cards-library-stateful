9df159eff83e78e88b8eebfec23d3f163d6100cf
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-08-02 23:23:40 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            subtitle: '',
+            title: '',
+            imagePath: '',
+            storyline: '',
+            rating: 0,
+            genre: 'action',
+        };
+    }
     render() {
         return (
             <div></div>
