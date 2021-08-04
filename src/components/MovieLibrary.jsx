32bf270e7fbbb3d3615abfa3f3685f16cf2359fa
Caê <eduardokae01@hotmail.com>
2021-08-04 10:36:41 -0300

    requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,38 +1,57 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
-
-    // const { movies } = this.props;
+    const { movies } = this.props;
 
     this.state = {
       searchText: '',
-      // bookmarkedOnly: false,
-      // selectedGenre: '',
-      // movies,
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  handleChange = ({ event }) => {
+    const { type, checked, value, name } = event;
+
+    const newChange = type === 'checkbox' ? checked : value;
+
+    this.setState = {
+      [name]: newChange,
     };
   }
 
   render() {
-    const { searchText } = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const { handleChange } = this;
 
     return (
       <div>
-        <SearchBar searchText={ searchText } />
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ handleChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ handleChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ handleChange }
+        />
+        <MovieList movies={ movies } />
       </div>
     );
   }
 }
 
-// MovieLibrary.propTypes = {
-//   movies: PropTypes.func,
-// };
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf,
+};
 
-// MovieLibrary.defaultProps = {
-//   movies: [],
-// };
+MovieLibrary.defaultProps = {
+  movies: [],
+};
 
 export default MovieLibrary;
