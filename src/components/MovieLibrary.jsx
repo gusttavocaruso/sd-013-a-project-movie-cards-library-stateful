e943be57415925f039b06a3794a70a64075fc5a3
Seu nomecd <canaldogildosantos@gmail.com>
2021-08-04 03:37:28 -0300

    finaliza desafio 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,52 +1,52 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
-import MovieList from './MovieList';
+// import PropTypes from 'prop-types';
+// import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
+// import MovieList from './MovieList';
 
 class MovieLibrary extends Component {
-  constructor() {
-    super();
-    const { movies } = this.props;
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      movies: { movies },
-    };
-  }
+  // constructor() {
+  //   super();
+  //   const { movies } = this.props;
+  //   this.state = {
+  //     searchText: '',
+  //     bookmarkedOnly: false,
+  //     selectedGenre: '',
+  //     movies: { movies },
+  //   };
+
   // change = (param) => {
   //   const { movies } = this.state;
   //   this.setState({ movies: [...movies, param] });
   // }
 
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    // const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
         <h2> My awesome movie library</h2>
-        <SearchBar
+        {/* <SearchBar
           searchText={ searchText }
           bookmarkedOnly={ bookmarkedOnly }
           selectedGenre={ selectedGenre }
         />
         <MovieList movies={ movies } />
-        <AddMovie />
+        <AddMovie /> */}
       </div>
     );
   }
 }
 
-MovieLibrary.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string,
-    subtitle: PropTypes.string,
-    storyline: PropTypes.string,
-    rating: PropTypes.number,
-    imagePath: PropTypes.string,
-    bookmarked: true,
-    genre: PropTypes.string,
-  })).isRequired,
-};
+// MovieLibrary.propTypes = {
+//   movies: PropTypes.arrayOf(PropTypes.shape({
+//     title: PropTypes.string,
+//     subtitle: PropTypes.string,
+//     storyline: PropTypes.string,
+//     rating: PropTypes.number,
+//     imagePath: PropTypes.string,
+//     bookmarked: true,
+//     genre: PropTypes.string,
+//   })).isRequired,
+// };
 
 export default MovieLibrary;
