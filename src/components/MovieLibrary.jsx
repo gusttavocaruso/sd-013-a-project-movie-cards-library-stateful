a39dd0d90913b2914d8c8d3556cdba79759ac11a
Fabiola Moutinho <fabiolamoutinho@hotmail.com>
2021-08-04 01:09:05 -0400

    Requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+
   render() {
+    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
-      <div />
+      <div>
+        <AddMovie value={ movies } />
+        { searchText }
+        { bookmarkedOnly }
+        { selectedGenre }
+        <MovieList movies={ movies } />
+        <SearchBar />
+      </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
 export default MovieLibrary;
