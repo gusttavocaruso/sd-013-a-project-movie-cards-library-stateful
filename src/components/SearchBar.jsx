b5c6cc3875ee0e78110cba0eaa074d74516808ba
Marcelo Alessandro <marceloouropreto@gmail.com>
2021-08-04 00:15:50 -0300

    Renderiza form dentro de SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <form data-testid="search-bar-form"></form>
+      </div>
     );
   }
 }
