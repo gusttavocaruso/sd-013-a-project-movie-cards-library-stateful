5cbf5d0958b78400cf80548632a8e8cdcd9bcbda
Marcelo Alessandro <marceloouropreto@gmail.com>
2021-08-04 00:01:50 -0300

    Requisito SearchBar recebe props e renderiza

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,6 @@
 // implement MovieLibrary component here
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
@@ -11,15 +11,42 @@ class MovieLibrary extends Component {
   }
 
   render() {
+    const { movies } = this.props;
+    const {
+      searchText, //uma string
+      onSearchTextChange, //uma callback
+      bookmarkedOnly, //um boolean
+      onBookmarkedChange, //uma callback
+      selectedGenre, //uma string
+      onSelectedGenreChange, //uma callback
+    } = movies;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={this.props.movies} />
+        <SearchBar 
+          searchText={ searchText }
+          onSearchTextChange={ onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ onSelectedGenreChange }
+        />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.PropTypes = {
+  movies: PropTypes.shape({
+    searchText: PropTypes.string,
+    onSearchTextChange: PropTypes.func,
+    bookmarkedOnly: PropTypes.bool,
+    onBookmarkedChange:PropTypes.func,
+    selectedGenre:PropTypes.string,
+    onSelectedGenreChange: PropTypes.func,
+  }).isRequired,
+};
 
 export default MovieLibrary;
