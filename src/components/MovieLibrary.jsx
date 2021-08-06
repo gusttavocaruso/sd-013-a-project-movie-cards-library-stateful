f62596591c9aab67a5139e6bfc531b3cea2e3fe1
Victor Diniz <victoralbertocostadiniz@gmail.com>
2021-08-06 13:37:27 -0300

    Adiciona um comentario pra testar uma coisa

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -21,6 +21,8 @@ class MovieLibrary extends React.Component {
     };
   }
 
+  // TESTE
+
   handleChange({ target }) {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
