bbca297b0e802e4719cb4f72e572040f1a123858
Amaury Queiroz Jr <amaury.tqj@gmail.com>
2021-08-07 13:51:58 -0300

    compentizando SearchBar.jsx

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movieList } = this.state;
     return (
       <div>
-        <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList />
+        <h2> Minha incrível Biblioteca de Filmes </h2>
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          onSearchTextChange={ this.handleChange }
+          onBookmarkedChange={ this.handleChange }
+          onSelectedGenreChange={ this.handleChange }
+        />
+        <MovieList movies={ movieList } />
         <AddMovie />
       </div>
     );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import SelectComponent from './Select';
+import InputComponent from './Inputs';
+import selectOptions from './searchOptionsSelect';
 
 class SearchBar extends React.Component {
   render() {
+    const {
+      onSearchTextChange,
+      searchText,
+      onBookmarkedChange,
+      bookmarkedOnly,
+      onSelectedGenreChange,
+      selectedGenre,
+    } = this.props;
+
     return (
-      <h1>Hello</h1>
+      <form data-testid="search-bar-form">
+        <InputComponent
+          label="Inclui o texto:"
+          stateName="searchText"
+          inputType="text"
+          identifier="text-input"
+          onChangeInput={ onSearchTextChange }
+          inputValue={ searchText }
+        />
+        <InputComponent
+          label="Mostrar somente favoritos"
+          stateName="bookmarkedOnly"
+          inputType="checkbox"
+          identifier="checkbox-input"
+          onChangeInput={ onBookmarkedChange }
+          checked={ bookmarkedOnly }
+          inputValue={ bookmarkedOnly }
+        />
+        <SelectComponent
+          label="Filtrar por gênero"
+          identifier="select"
+          getValue={ selectedGenre }
+          onChangeComponent={ onSelectedGenreChange }
+          options={ selectOptions }
+          name="selectedGenre"
+        />
+      </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+};
 
 export default SearchBar;
