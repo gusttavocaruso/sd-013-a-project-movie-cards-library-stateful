f51175e180299e5a609095465a47754a15899ad6
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-08-07 14:03:04 -0300

    Requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,32 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
+      subtitle: '',
       title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
     };
+
+    this.changeStates = this.changeStates.bind(this);
+  }
+
+  changeStates(event) {
+    const { name, value } = event.target;
+    this.setValue({
+      [name]: value,
+    });
   }
 
   render() {
-    const { title } = this.state;
+    const { onClick } = this.props;
+    const {
+      subtitle,
+      title,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.state;
+
     return (
-      <div>
-        <form data-testid="add-movie-form" />
-        <label data-testid="title-input-label" htmlFor="titulo">
+      <form data-testid="add-movie-form" >
+        <label data-testid="title-input-label" htmlFor="title">
           Título
           <input
-            id="titulo"
+            id="title"
             type="text"
             data-testid="title-input"
-            value={ title.value }
-            onChange={ title }
+            value={ title }
+            onChange={ this.changeStates }
           />
         </label>
-      </div>
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            id="subtitle"
+            type="text"
+            data-testid="subtitle-input"
+            value={ subtitle }
+            onChange={ this.changeStates }
+          />
+        </label>
+      </form>
     );
   }
 }
+
+AddMovie.propTypes = {
+  callback: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
