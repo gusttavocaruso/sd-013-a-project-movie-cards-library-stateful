2ca4bb7dca8c8f2db55c52016cd29a303d12697c
Micael Santos <micaelmaicon@hotmail.com>
2021-08-10 14:40:01 -0300

    Requeriments in process

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,52 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import ContentCard from './ContentCard';
+import HeaderCard from './HeaderCard';
+
+export default class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      // storyline: '',
+      // genre: 'action',
+      // rating: 0,
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  // handleChangeRating = (e) => {
+  //   this.setState({
+  //     rating: e.target.value,
+  //   });
+  // }
+
+  render() {
+    const {
+      title,
+      subtitle,
+      imagePath,
+      // storyline,
+      // genre,
+      // rating,
+    } = this.state;
+
+    return (
+      <form data-testid="add-movie-form">
+        <HeaderCard
+          title={ title }
+          subtitle={ subtitle }
+          handleChange={ this.handleChange }
+        />
+        <ContentCard imagePath={ imagePath } handleChange={ this.handleChange }  />
+      </form>
+    );
+  }
+}
