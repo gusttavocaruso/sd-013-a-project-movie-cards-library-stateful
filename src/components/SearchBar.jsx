7341c25abac368fdb26872e8762674495990e74d
igor <igorhdsouza@gmail.com>
2021-08-14 11:37:53 -0300

    req 02

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      ''
+      <form data-testid="search-bar-form">
+        <label htmlFor="searchInput">
+          <input
+            type="text"
+            name="searchInput"
+            data-testid="text-input"
+            searchText=""
+          />
+        </label>
+      </form>
     );
   }
 }
