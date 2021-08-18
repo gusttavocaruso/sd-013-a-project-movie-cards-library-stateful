2c2c6ec8f26c9ac5f96254070207bf1d535532eb
Rodrigo Pova <rodrigopova94@gmail.com>
2021-08-18 18:43:06 -0300

    finaliza requisitos 15 a 19

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
-// import MovieList from './MovieList';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
@@ -14,6 +15,30 @@ class MovieLibrary extends React.Component {
     };
   }
 
+  search = () => {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+
+    const textSearch = movies.filter((movie) => (
+      movie.title.toLowerCase().includes(searchText.toLowerCase())
+      || movie.subtitle.toLowerCase().includes(searchText.toLowerCase())
+      || movie.storyline.toLowerCase().includes(searchText.toLowerCase())
+    ));
+
+    const bookmarked = bookmarkedOnly
+      ? textSearch.filter((movie) => movie.bookmarked) : textSearch;
+
+    const searchResults = selectedGenre !== ''
+      ? bookmarked.filter((movie) => movie.genre === selectedGenre) : bookmarked;
+
+    return searchResults;
+  }
+
+  handleChange = (event) => {
+    const { target } = event;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [target.name]: value });
+  }
+
   addMovie = (movie) => {
     const { movies } = this.state;
 
@@ -21,14 +46,27 @@ class MovieLibrary extends React.Component {
   }
 
   render() {
+    const searchResults = this.search();
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <main>
-        <SearchBar />
-        {/* <MovieList /> */}
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleChange }
+        />
+        <MovieList movies={ searchResults } />
         <AddMovie onClick={ this.addMovie } />
       </main>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieLibrary;
