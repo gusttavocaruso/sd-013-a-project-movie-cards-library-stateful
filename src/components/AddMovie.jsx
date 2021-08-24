c2d5a1202f6636ca3d86dd284f66cea1461f4824
Bruno Pinho <brunopinho@outlook.com>
2021-08-24 12:31:16 -0300

    feito requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
+const INITIAL_STATE = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
+
 class AddMovie extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
   }
 
   render() {
+    const { onClick } = this.props;
+
     return (
       <p>teste</p>
     );
