37dfc56a618dfaa291e119c2ab24219cfcb871fb
Bruno Pinho <brunopinho@outlook.com>
2021-08-24 12:17:53 -0300

    feito requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -48,6 +48,26 @@ class SearchBar extends React.Component {
             data-testid="checkbox-input"
           />
         </label>
+
+        {/* Req. 5 */}
+        <label
+          htmlFor="genre-select"
+          data-testid="select-input-label"
+        >
+          Filtrar por gênero:
+          <select
+            name="genres"
+            id="genre-select"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
