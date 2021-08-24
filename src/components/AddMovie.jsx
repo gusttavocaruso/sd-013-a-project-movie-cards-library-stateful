e73465adbd5178a78a85b1d5c739b02618ec8f89
Bruno Pinho <brunopinho@outlook.com>
2021-08-24 12:25:18 -0300

    feito requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,15 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <p>teste</p>
+    );
+  }
+}
+
+export default AddMovie;
