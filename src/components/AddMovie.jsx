f646945dc40c614b8896e8ad42662d231275c62a
Bruno Pinho <brunopinho@outlook.com>
2021-08-24 12:46:12 -0300

    feito requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -23,7 +23,7 @@ class AddMovie extends React.Component {
 
   render() {
     const { onClick } = this.props;
-    const { title } = this.state;
+    const { title, subtitle } = this.state;
 
     return (
       <form data-testid="add-movie-form">
@@ -39,11 +39,27 @@ class AddMovie extends React.Component {
             id="title-input"
             placeholder="Insira o título do filme"
             value={ title }
-            onChange={ (event) => this.updateState('title', event.target.value) }
+            onChange={(event) => this.updateState('title', event.target.value)}
             data-testid="title-input"
           />
         </label>
 
+        {/* Req. 9 */}
+        <label
+          htmlFor="subtitle-input"
+          data-testid="subtitle-input-label"
+        >
+          Subtítulo:
+          <input
+            type="text"
+            name="subtitle-input"
+            id="subtitle-input"
+            placeholder="Insira o subtítulo do filme"
+            value={ subtitle }
+            onChange={(event) => this.updateState('subtitle', event.target.value)}
+            data-testid="subtitle-input"
+          />
+        </label>
       </form>
     );
   }
