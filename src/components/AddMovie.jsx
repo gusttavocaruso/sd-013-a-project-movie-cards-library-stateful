862cafda6bdbb6699735ffede3ac398a4612ee89
Bruno Pinho <brunopinho@outlook.com>
2021-08-24 12:42:19 -0300

    feito requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -16,11 +16,33 @@ class AddMovie extends React.Component {
     this.state = { ...INITIAL_STATE };
   }
 
+  // Inspirada na sintaxe do componente MovieForm do projeto Movie Card Library CRUD
+  updateState(field, newValue) {
+    this.setState({ [field]: newValue });
+  }
+
   render() {
     const { onClick } = this.props;
+    const { title } = this.state;
 
     return (
       <form data-testid="add-movie-form">
+        {/* Req. 8 */}
+        <label
+          htmlFor="title-input"
+          data-testid="title-input-label"
+        >
+          Título:
+          <input
+            type="text"
+            name="title"
+            id="title-input"
+            placeholder="Insira o título do filme"
+            value={ title }
+            onChange={ (event) => this.updateState('title', event.target.value) }
+            data-testid="title-input"
+          />
+        </label>
 
       </form>
     );
