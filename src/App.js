f1196909be40e694bd998856c095466f62fc6f88
Bruno Pinho <brunopinho@outlook.com>
2021-08-25 12:14:01 -0300

    feito requisito 17

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
 import './App.css';
-import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <AddMovie />
+      <MovieLibrary />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
@@ -14,9 +15,37 @@ class MovieLibrary extends React.Component {
     };
   }
 
+  onSearchTextChange(event) {
+    this.setState({
+      searchText: event.target.value,
+    });
+    console.log(this.state.searchText);
+  }
+
+  onBookmarkedChange(event) {
+    this.setState({
+      bookmarkedOnly: event.target.checked,
+    });
+    console.log(this.state);
+  }
+
+  onSelectedGenreChange(event) {
+    this.setState({
+      selectedGenre: event.target.value,
+    });
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
-      <p>teste</p>
+      <SearchBar
+        searchText={ searchText }
+        onSearchTextChange={ (event) => this.onSearchTextChange(event) }
+        bookmarkedOnly={ bookmarkedOnly }
+        onBookmarkedChange={ (event) => this.onBookmarkedChange(event) }
+        selectedGenre={ selectedGenre }
+        onSelectedGenreChange={ (event) => this.onSelectedGenreChange(event) }
+      />
     );
   }
 }
