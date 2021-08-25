0585d71b9b77a4561ac61e86542f4b9691e5e74e
Bruno Pinho <brunopinho@outlook.com>
2021-08-25 15:52:45 -0300

    feito requisito 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
@@ -35,17 +36,47 @@ class MovieLibrary extends React.Component {
     });
   }
 
+  // Com auxílio dos códigos do Notion da turma e do colega Vinicius Dionysio (https://www.notion.so/Projeto-Movie-Cards-Library-Stateful-95781cdbc8eb4f4b871505ab952495ba e https://github.com/tryber/sd-013-a-project-movie-cards-library-stateful/pull/41/files)
+  onMoviesSearched() {
+    const { movies, bookmarkedOnly, selectedGenre, searchText } = this.state;
+
+    let filteredMoviesList = movies;
+
+    if (bookmarkedOnly) {
+      filteredMoviesList = movies.filter((movie) => movie.bookmarked);
+    }
+
+    if (selectedGenre !== '') {
+      filteredMoviesList = movies.filter((movie) => movie.genre === selectedGenre);
+    }
+
+    if (searchText !== '') {
+      filteredMoviesList = movies.filter((movie) => {
+        return movie.title.toLowerCase().includes(searchText.toLocaleLowerCase())
+        || movie.subtitle.toLowerCase().includes(searchText.toLocaleLowerCase())
+        || movie.storyline.toLowerCase().includes(searchText.toLocaleLowerCase());
+      });
+    }
+
+    return filteredMoviesList;
+  }
+
   render() {
     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
-      <SearchBar
-        searchText={ searchText }
-        onSearchTextChange={ (event) => this.onSearchTextChange(event) }
-        bookmarkedOnly={ bookmarkedOnly }
-        onBookmarkedChange={ (event) => this.onBookmarkedChange(event) }
-        selectedGenre={ selectedGenre }
-        onSelectedGenreChange={ (event) => this.onSelectedGenreChange(event) }
-      />
+      <main>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ (event) => this.onSearchTextChange(event) }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ (event) => this.onBookmarkedChange(event) }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ (event) => this.onSelectedGenreChange(event) }
+        />
+        <MovieList
+          movies={ this.onMoviesSearched() }
+        />
+      </main>
     );
   }
 }
