ac646f554312dfd66ebcd6a786bd291a2945ef36
João Victor Paduam <joao.paduam@hotmail.com>
2021-08-28 17:11:14 -0300

    Inicio do Projeto

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,65 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import { string, func, bool } from 'prop-types';
+import SelectBox from './SelectBox';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <form
+        data-testid="search-bar-form"
+        action=""
+      >
+        <label
+          htmlFor="searchBox"
+          data-testid="text-input-label"
+        >
+          Inclui o texto
+          <input
+            id="searchBox"
+            type="text"
+            data-testid="text-imput"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <label
+          data-testid="checkbox-input-label"
+          htmlFor="onlyFavorites"
+        >
+          Mostrar somente favoritos
+          <input
+            data-textid="checkbox-input"
+            id="onlyFavorites"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+        <SelectBox
+          selectGenre={ selectedGenre }
+          onSelectedGenreChange={ onSelectedGenreChange }
+        />
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: string.isRequired,
+  onSearchTextChange: func.isRequired,
+  bookmarkedOnly: bool.isRequired,
+  onBookmarkedChange: func.isRequired,
+  selectedGenre: string.isRequired,
+  onSelectedGenreChange: func.isRequired,
+
+};
+
+export default SearchBar;
