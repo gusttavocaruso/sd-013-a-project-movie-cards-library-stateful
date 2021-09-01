9f21f8069cfea94a941dbc5d3f9811f9eb04b6dc
Max Salewski <maxsalewski@gmail.com>
2021-09-01 04:35:42 -0300

    Requisitos 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,35 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      subtitle: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+      imagePath: '',
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { title, subtitle, storyline, rating, genre, imagePath } = this.state;
+    const { onClick } = this.props;
+    return (
+      <form onSubmit={ this.handleSubmit } data-testid="add-movie-form" />
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -28,6 +28,21 @@ class SearchBar extends React.Component {
             data-testid="checkbox-input"
           />
         </label>
+        <label htmlFor="search-bar-form" data-testid="select-input-label">
+          Filtrar por gênero:
+          <select
+            name="selectedGenre"
+            id="selectedGenre"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
