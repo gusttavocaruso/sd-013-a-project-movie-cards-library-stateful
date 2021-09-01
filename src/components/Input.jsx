39c13dfd445d0611ce61f14beb04ff20cf08ddbc
Yuri <yurifranco18@live.com>
2021-09-01 05:18:22 -0300

    foi feito componente Input para o SearchBar

---
+++ src/components/Input.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Input extends Component {
+  render() {
+    const {
+      labelText,
+      labelDataTestId,
+      value,
+      checked,
+      onChange,
+      inputDataTestId,
+      nome,
+      inputType,
+    } = this.props;
+
+    return (
+      <label
+        htmlFor={ nome }
+        data-testid={ labelDataTestId }
+      >
+        { labelText }
+        <input
+          data-testid={ inputDataTestId }
+          type={ inputType }
+          name={ nome }
+          value={ value }
+          onChange={ onChange }
+          checked={ checked }
+        />
+      </label>
+    );
+  }
+}
+
+Input.propTypes = {
+  labelTest: PropTypes.string,
+  labelDataTestId: PropTypes.string,
+  value: PropTypes.string,
+  checked: PropTypes.bool,
+  onChange: PropTypes.func,
+  inputDataTestId: PropTypes.func,
+  nome: PropTypes.string,
+  inputType: PropTypes.string,
+}.isRequired;
+
+export default Input;
