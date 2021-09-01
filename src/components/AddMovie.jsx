cea12e4ed4d62b65d47ad3842c36188e2459c4a4
Max Salewski <maxsalewski@gmail.com>
2021-09-01 06:08:59 -0300

    Tentanto Fix erro lint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,13 +1,12 @@
-/* eslint-disable import/extensions */
 import React from 'react';
 import PropTypes from 'prop-types';
-import TitleInput from './movieForm/TitleInput';
-import SubtitleInput from './movieForm/SubtitleInput';
-import ImageURLInput from './movieForm/ImageURLInput';
-import StoryLineInput from './movieForm/StoryLineInput';
-import RatingInput from './movieForm/RatingInput';
-import GenreInput from './movieForm/GenreInput';
-import SubmitForm from './movieForm/SubmitForm';
+import TitleInput from './TitleInput';
+import SubtitleInput from './SubtitleInput';
+import ImageURLInput from './ImageURLInput';
+import StoryLineInput from './StoryLineInput';
+import RatingInput from './RatingInput';
+import GenreInput from './GenreInput';
+import SubmitForm from './SubmitForm';
 
 class AddMovie extends React.Component {
   constructor(props) {
