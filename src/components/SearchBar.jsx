0511d4efd545936c0fe51b72fffe0c18b5259c21
Yuri <yurifranco18@live.com>
2021-09-01 04:12:07 -0300

    foi resolvido erro da linha 15 faltam os props validation

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,17 +3,25 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-      const {
-          searchText,
-          onSearchTextChange,
-          bookmarkedOnly,
-          onBookmarkedChange,
-          selectedGenre,
-          onSelectedGenChange,
-      } = this.props
-    return (
-    );
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return <form data-testid="search-bar-form" />;
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
+}.isRequired;
 
 export default SearchBar;
